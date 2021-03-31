d702c013feb5caa3eef6c519f979152f6ca0ed56
Alonso del Arte <alonso.delarte@gmail.com>
2021-03-31 17:53:17 -0400

    Oops, meant Knight, not Bishop

--- src/game/pieces/PromotedKnight.scala
+++ src/game/pieces/PromotedKnight.scala
@@ -2,7 +2,7 @@ package game.pieces
 
 import game.Player
 
-class PromotedKnight(val pawn: Pawn) extends Bishop with Promoted {
+class PromotedKnight(val pawn: Pawn) extends Knight with Promoted {
   override val affiliation: Player = pawn.affiliation
 
   override def prior: Pawn = pawn
