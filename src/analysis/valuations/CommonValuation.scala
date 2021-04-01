f41a18ae01f1a4495dffe15aad9691fe31b520e4
Alonso del Arte <alonso.delarte@gmail.com>
2021-04-01 01:27:48 -0400

    Now passing specific valuation tests

--- src/analysis/valuations/CommonValuation.scala
+++ src/analysis/valuations/CommonValuation.scala
@@ -1,6 +1,9 @@
 package analysis.valuations
 
 import analysis.Centipawns
-import game.pieces.Piece
-// THIS WILL FAIL THE FIRST TEST
-object CommonValuation extends ConstantCentipawnValuation(Map(classOf[Piece] -> Centipawns(-250)))
+import game.pieces.{Bishop, King, Knight, Pawn, Queen, Rook}
+
+object CommonValuation extends ConstantCentipawnValuation(Map(classOf[Pawn] ->
+  Centipawns(100), classOf[Rook] -> Centipawns(500), classOf[Knight] ->
+  Centipawns(300), classOf[Bishop] -> Centipawns(300), classOf[Queen] ->
+  Centipawns(900), classOf[King] -> Centipawns(0)))
