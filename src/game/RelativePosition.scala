9f78527535289ceeca4cc16930aee2842a7b48e9
Alonso del Arte <alonso.delarte@gmail.com>
2021-04-08 23:49:44 -0400

    Three stubs

--- src/game/RelativePosition.scala
+++ src/game/RelativePosition.scala
@@ -4,4 +4,15 @@ class RelativePosition(val offsetX: Int, val offsetY: Int) {
 
   override def toString: String = s"(${this.offsetX}, ${this.offsetY})"
 
+  // STUB TO FAIL THE FIRST TEST
+  override def equals(obj: Any): Boolean = false
+
+  // STUB TO FAIL THE FIRST TEST
+  override def hashCode: Int = 0
+
+  // STUB TO FAIL THE FIRST TEST
+  def abs: Int = -1
+
+  // STUB TO FAIL THE FIRST TEST
+
 }
