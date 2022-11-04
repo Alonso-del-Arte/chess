b1cf3022f7b111e7007ecfa26e0b036d3ca966a2
Alonso del Arte <alonso.delarte@gmail.com>
2022-11-04 17:42:12 -0400

    Refactoring to take advantage of inherited field

--- src/game/pieces/Knight.scala
+++ src/game/pieces/Knight.scala
@@ -5,6 +5,5 @@ import game.RelativePositionRange
 abstract class Knight extends Piece {
   override val possibleMoves: Set[RelativePositionRange] = Set()
   override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = true
 
 }
