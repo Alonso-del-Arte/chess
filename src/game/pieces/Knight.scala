e08034f89d74b8bc4953ab848c60a9716f63f6e3
Alonso del Arte <alonso.delarte@gmail.com>
2022-11-04 00:09:11 -0400

    Passing canJumpOver( ) test

--- src/game/pieces/Knight.scala
+++ src/game/pieces/Knight.scala
@@ -4,5 +4,6 @@ import game.RelativePositionRange
 
 abstract class Knight extends Piece {
   override val possibleMoves: Set[RelativePositionRange] = Set()
+  override val canJumpOver: Boolean = true
 
 }
--- test/game/pieces/KnightTest.scala
+++ test/game/pieces/KnightTest.scala
@@ -7,6 +7,13 @@ import org.junit.jupiter.api.Test
 
 class KnightTest {
 
+  @Test def testCanJumpOver(): Unit = {
+    println("canJumpOver")
+    val knight = new KnightImpl
+    assert(knight.canJumpOver,
+      "Knights should be able to jump over other pieces")
+  }
+
   private class KnightImpl extends Knight {
     override val affiliation: Player = Neutral
   }
