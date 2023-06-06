13d6a68470b119ef9ff5c420e5abed03dd7e6758
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-05 23:58:52 -0400

    New file

---
+++ src/notation/ForsythEdwardsNotation.scala
@@ -0,0 +1,13 @@
+package notation
+
+import game.BoardState
+
+object ForsythEdwardsNotation {
+
+//  def parseFEN(s: String): ForsythEdwardsNotation
+
+}
+
+class ForsythEdwardsNotation(val state: BoardState) {
+
+}
---
+++ test/notation/ForsythEdwardsNotationTest.scala
@@ -0,0 +1,8 @@
+package notation
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class ForsythEdwardsNotationTest {
+
+}
