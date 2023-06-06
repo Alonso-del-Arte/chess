d9e9f6f5f1425d69737ca4ec2a991b9e2adfc5b4
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-06 00:16:20 -0400

    Failing parse FEN of initial board state test

--- src/notation/ForsythEdwardsNotation.scala
+++ src/notation/ForsythEdwardsNotation.scala
@@ -1,10 +1,15 @@
 package notation
 
-import game.BoardState
+import game.{BoardState, RegularBoard, RegularPosition}
 
 object ForsythEdwardsNotation {
 
-//  def parseFEN(s: String): ForsythEdwardsNotation
+  def parseFEN(s: String): ForsythEdwardsNotation = {
+    val boardState = new BoardState(RegularBoard, game.Black,
+      Map(new RegularPosition(1, 1) -> game.pieces.white.QueenSideRook,
+      new RegularPosition(2, 1) -> game.pieces.white.QueenSideKnight))
+    new ForsythEdwardsNotation(boardState)
+  }
 
 }
 
--- test/notation/ForsythEdwardsNotationTest.scala
+++ test/notation/ForsythEdwardsNotationTest.scala
@@ -1,8 +1,18 @@
 package notation
 
+import game.{BoardState, InitialBoardState}
+
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
 
 class ForsythEdwardsNotationTest {
+
+  @Test
+  def testParseFENInitialBoardState(): Unit = {
+    val s = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    val expected = InitialBoardState
+    val actual = ForsythEdwardsNotation.parseFEN(s).state
+    assertEquals(expected, actual)
+  }
 
 }
