e4418fa021dc3ff6b4c9e67f942da426f9bb4123
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-06 23:19:42 -0400

    Failing referential equality test

--- test/game/RelativePositionRangeTest.scala
+++ test/game/RelativePositionRangeTest.scala
@@ -22,4 +22,18 @@ class RelativePositionRangeTest {
     assertEquals(expected, actual)
   }
 
+  @Test
+  def testReferentialEquality(): Unit = {
+    val stepX = Random.nextInt(16) - 8
+    val stepY = Random.nextInt(16) - 8
+    val startX = Random.nextInt(32) - 16
+    val finishX = startX + stepX
+    val startY = Random.nextInt(32) - 16
+    val finishY = startY + stepY
+    val start = new RelativePosition(startX, startY)
+    val finish = new RelativePosition(finishX, finishY)
+    val range = new RelativePositionRange(start, finish)
+    assertEquals(range, range)
+  }
+
 }
