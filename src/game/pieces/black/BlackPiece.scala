797d5f2e70b7b3ed924176e6704135f0ee8996f6
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-09 23:26:40 -0400

    Prep to fail next few tests

--- src/game/pieces/black/BlackPiece.scala
+++ src/game/pieces/black/BlackPiece.scala
@@ -1,9 +1,9 @@
 package game.pieces.black
 
-import game.{Black, Player}
+import game.{Black, Neutral, Player}
 import game.pieces.Piece
 
 trait BlackPiece extends Piece {
-  override val affiliation: Player = Black
+  override val affiliation: Player = Neutral
 
 }
--- src/game/pieces/white/WhitePiece.scala
+++ src/game/pieces/white/WhitePiece.scala
@@ -1,9 +1,9 @@
 package game.pieces.white
 
-import game.{Player, White}
+import game.{Neutral, Player, White}
 import game.pieces.Piece
 
 trait WhitePiece extends Piece {
-  override val affiliation: Player = White
+  override val affiliation: Player = Neutral
 
 }
