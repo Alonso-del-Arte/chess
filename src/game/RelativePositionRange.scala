6c391dec9b8fcb28d3b0b6e2e2f049e88f848f24
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-13 00:59:38 -0400

    Passing toString( ) test

--- src/game/RelativePositionRange.scala
+++ src/game/RelativePositionRange.scala
@@ -1,7 +1,6 @@
 package game
 
 object RelativePositionRange {
-  private val STAY_PUT: RelativePosition = new RelativePosition(0, 0)
 
   def determineStep(start: RelativePosition,
                     finish: RelativePosition): RelativePosition = {
@@ -10,7 +9,6 @@ object RelativePositionRange {
   }
 
   def apply(startAndEnd: RelativePosition): RelativePositionRange = {
-    val transposed = new RelativePosition(startAndEnd.offsetY, startAndEnd.offsetX)
     new RelativePositionRange(startAndEnd, startAndEnd)
   }
 
@@ -21,6 +19,9 @@ class RelativePositionRange(val start: RelativePosition,
 
   // TODO: Write tests for this
   def apply(index: Int): RelativePosition = new RelativePosition(0, 0)
+
+  override def toString: String =
+    s"${this.start.toString} to ${this.finish.toString}"
 
   override def equals(obj: Any): Boolean = obj match {
     case other: RelativePositionRange => (this.start == other.start) &&
