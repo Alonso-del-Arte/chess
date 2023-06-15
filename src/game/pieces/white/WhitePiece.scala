2215543ba5e38d4eda71e6eb3b4aca2c593754d5
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-15 01:43:34 -0400

    Passing affiliation test

--- src/game/pieces/white/WhitePiece.scala
+++ src/game/pieces/white/WhitePiece.scala
@@ -1,9 +1,9 @@
 package game.pieces.white
 
-import game.{Neutral, Player, White}
+import game.{Player, White}
 import game.pieces.Piece
 
 trait WhitePiece extends Piece {
-  override val affiliation: Player = Neutral
+  override val affiliation: Player = White
 
 }
