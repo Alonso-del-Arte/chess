0ff338b6e9bf153574500c53c75ed9aae1472e51
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-15 01:01:18 -0400

    Passing all tests so far

--- src/game/pieces/white/WhitePawn.scala
+++ src/game/pieces/white/WhitePawn.scala
@@ -1,10 +1,17 @@
 package game.pieces.white
 
-import game.{RelativePosition, RelativePositionRange}
+import game.{Player, RelativePosition, RelativePositionRange, White}
 import game.pieces.Pawn
 
 abstract class WhitePawn extends Pawn with WhitePiece {
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(0, 1)))
+  override val affiliation: Player = White
+  override val possibleMoves: Set[RelativePositionRange]
+  = Set(new RelativePositionRange(new RelativePosition(0, 1),
+    new RelativePosition(0, 2)))
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = false
+  override val possibleCaptures: Set[RelativePositionRange]
+  = Set(RelativePositionRange(new RelativePosition(-1, 1)),
+    RelativePositionRange(new RelativePosition(1, 1)))
 
 }
