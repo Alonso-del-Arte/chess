a1e339d33fcb3556f91674ef4c29a4effb55bcfd
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-16 23:59:15 -0400

    Passing all tests so far

--- src/game/pieces/black/KingSideBishopPawn.scala
+++ src/game/pieces/black/KingSideBishopPawn.scala
@@ -1,3 +1,16 @@
 package game.pieces.black
 
-object KingSideBishopPawn extends BlackPawn with BlackPiece
+import game.{Black, Player, RelativePosition, RelativePositionRange}
+
+object KingSideBishopPawn extends BlackPawn with BlackPiece {
+  override val affiliation: Player = Black
+  override val possibleMoves: Set[RelativePositionRange]
+  = Set(new RelativePositionRange(new RelativePosition(0, -1),
+    new RelativePosition(0, -2)))
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = false
+  override val possibleCaptures: Set[RelativePositionRange]
+  = Set(RelativePositionRange(new RelativePosition(-1, -1)),
+    RelativePositionRange(new RelativePosition(1, -1)))
+
+}
--- src/game/pieces/black/QueenPawn.scala
+++ src/game/pieces/black/QueenPawn.scala
@@ -1,8 +1,21 @@
 package game.pieces.black
 
+import game.{Black, Player, RelativePosition, RelativePositionRange}
+
 /**
  * The pawn in front of the queen at the beginning of the game. Not to be
  * confused with [[game.pieces.PromotedQueen]], a pawn that is promoted to a
  * queen.
  */
-object QueenPawn extends BlackPawn with BlackPiece
+object QueenPawn extends BlackPawn with BlackPiece {
+  override val affiliation: Player = Black
+  override val possibleMoves: Set[RelativePositionRange]
+    = Set(new RelativePositionRange(new RelativePosition(0, -1),
+      new RelativePosition(0, -2)))
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = false
+  override val possibleCaptures: Set[RelativePositionRange]
+    = Set(RelativePositionRange(new RelativePosition(-1, -1)),
+      RelativePositionRange(new RelativePosition(1, -1)))
+
+}
