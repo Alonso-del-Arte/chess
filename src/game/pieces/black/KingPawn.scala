2d717f5c470ed7c6cc7fc3814aa2e2dd553c5ec0
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-17 02:22:33 -0400

    Refactored

--- src/game/pieces/black/KingPawn.scala
+++ src/game/pieces/black/KingPawn.scala
@@ -1,16 +1,3 @@
 package game.pieces.black
 
-import game.{Black, Player, RelativePosition, RelativePositionRange}
-
-object KingPawn extends BlackPawn with BlackPiece {
-  override val affiliation: Player = Black
-  override val possibleMoves: Set[RelativePositionRange]
-    = Set(new RelativePositionRange(new RelativePosition(0, -1),
-      new RelativePosition(0, -2)))
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = false
-  override val possibleCaptures: Set[RelativePositionRange]
-    = Set(RelativePositionRange(new RelativePosition(-1, -1)),
-      RelativePositionRange(new RelativePosition(1, -1)))
-
-}
+object KingPawn extends BlackPawn with BlackPiece
