dd84bf8c70d00279e9055bc8513aaaabec2d3aad
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-17 00:25:06 -0400

    Failing tests

--- test/game/pieces/black/BlackPawnTest.scala
+++ test/game/pieces/black/BlackPawnTest.scala
@@ -1,10 +1,47 @@
 package game.pieces.black
 
+import game.{Black, RelativePosition, RelativePositionRange}
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
 
 class BlackPawnTest {
 
-  // PLACEHOLDER
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    assertEquals(Black, BlackPawnImpl.affiliation)
+  }
+
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val start = new RelativePosition(0, -1)
+    val finish = new RelativePosition(0, -2)
+    val expected = Set(new RelativePositionRange(start, finish))
+    val actual = BlackPawnImpl.possibleMoves
+    assertEquals(expected, actual)
+  }
+
+  @Test def testCanJumpOver(): Unit = {
+    println("canJumpOver")
+    val msg = "Pawn shouldn't be able to jump over"
+    assert(!BlackPawnImpl.canJumpOver, msg)
+  }
+
+  @Test def testCaptureSameAsMove(): Unit = {
+    println("captureSameAsMove")
+    val msg = "Pawn shouldn't capture same as move"
+    assert(!BlackPawnImpl.captureSameAsMove, msg)
+  }
+
+  @Test def testPossibleCaptures(): Unit = {
+    println("possibleCaptures")
+    val captureLeft = new RelativePosition(-1, -1)
+    val captureRight = new RelativePosition(1, -1)
+    val expected = Set(RelativePositionRange(captureLeft),
+      RelativePositionRange(captureRight))
+    val actual = BlackPawnImpl.possibleCaptures
+    assertEquals(expected, actual)
+  }
+
+  private object BlackPawnImpl extends BlackPawn
 
 }
