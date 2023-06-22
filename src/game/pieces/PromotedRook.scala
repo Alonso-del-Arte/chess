a3d752b2abb23f7b7faf63c60e7f6fe94c2257e9
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-22 01:36:38 -0400

    Refactored

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,20 +4,6 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
-    new RelativePosition(0, 7))
-  private val right = new RelativePositionRange(new RelativePosition(1, 0),
-    new RelativePosition(7, 0))
-  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
-    new RelativePosition(0, -7))
-  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
-    new RelativePosition(-7, 0))
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(forwards, right, backwards, left)
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = true
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(forwards, right, backwards, left)
   override def prior: Pawn = pawn
 
 }
--- src/game/pieces/white/QueenSideRook.scala
+++ src/game/pieces/white/QueenSideRook.scala
@@ -1,23 +1,9 @@
 package game.pieces.white
 
-import game.{Player, RelativePosition, RelativePositionRange, White}
+import game.{Player, White}
 import game.pieces.Rook
 
 object QueenSideRook extends Rook with WhitePiece {
   override val affiliation: Player = White
-  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
-    new RelativePosition(0, 7))
-  private val right = new RelativePositionRange(new RelativePosition(1, 0),
-    new RelativePosition(7, 0))
-  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
-    new RelativePosition(0, -7))
-  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
-    new RelativePosition(-7, 0))
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(forwards, right, backwards, left)
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = true
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(forwards, right, backwards, left)
 
 }
