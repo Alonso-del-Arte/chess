897ab6adcf0d1911eb28bcebfd5b1123a7cda282
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-22 01:15:03 -0400

    Passing all tests so far

--- src/game/pieces/Rook.scala
+++ src/game/pieces/Rook.scala
@@ -3,11 +3,19 @@ package game.pieces
 import game.{RelativePosition, RelativePositionRange}
 
 abstract class Rook extends Piece {
-  override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = false
+  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
+    new RelativePosition(0, 7))
+  private val right = new RelativePositionRange(new RelativePosition(1, 0),
+    new RelativePosition(7, 0))
+  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
+    new RelativePosition(0, -7))
+  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
+    new RelativePosition(-7, 0))
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(forwards, right, backwards, left)
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = true
   override val possibleCaptures: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, 0),
-      new RelativePosition(8, 8)))
+    Set(forwards, right, backwards, left)
 
 }
