1f8d2fefdfb64541db6516bb05a38a87f3cc80ec
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-22 00:58:10 -0400

    Passing all tests so far

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,12 +4,20 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = false
+  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
+    new RelativePosition(0, 7))
+  private val right = new RelativePositionRange(new RelativePosition(1, 0),
+    new RelativePosition(7, 0))
+  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
+    new RelativePosition(0, -7))
+  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
+    new RelativePosition(-7, 0))
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(forwards, right, backwards, left)
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = true
   override val possibleCaptures: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, 0),
-      new RelativePosition(8, 8)))
+    Set(forwards, right, backwards, left)
   override def prior: Pawn = pawn
 
 }
