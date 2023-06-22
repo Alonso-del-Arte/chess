55f6ea2d5a277beba01392f17bda7ca23df3a34e
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-22 00:57:38 -0400

    Failing tests

--- test/game/pieces/PromotedRookTest.scala
+++ test/game/pieces/PromotedRookTest.scala
@@ -1,5 +1,7 @@
 package game.pieces
 
+import game.{RelativePosition, RelativePositionRange}
+
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
 
@@ -14,6 +16,52 @@ class PromotedRookTest {
       promotedRookWhite.affiliation)
     assertEquals(game.pieces.black.QueenSideRookPawn.affiliation,
       promotedRookBlack.affiliation)
+  }
+
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val forwards = new RelativePositionRange(new RelativePosition(0, 1),
+      new RelativePosition(0, 7))
+    val right = new RelativePositionRange(new RelativePosition(1, 0),
+      new RelativePosition(7, 0))
+    val backwards = new RelativePositionRange(new RelativePosition(0, -1),
+      new RelativePosition(0, -7))
+    val left = new RelativePositionRange(new RelativePosition(-1, 0),
+      new RelativePosition(-7, 0))
+    val rook = new PromotedRook(game.pieces.darkgray.HorsesPawn)
+    val expected = Set(forwards, right, backwards, left)
+    val actual = rook.possibleMoves
+    assertEquals(expected, actual)
+  }
+
+  @Test def testCanJumpOver(): Unit = {
+    println("canJumpOver")
+    val msg = "Rook shouldn't be able to jump over"
+    val rook = new PromotedRook(game.pieces.lightgray.TowersPawn)
+    assert(!rook.canJumpOver, msg)
+  }
+
+  @Test def testCaptureSameAsMove(): Unit = {
+    println("captureSameAsMove")
+    val msg = "Rook should capture same as move"
+    val rook = new PromotedRook(game.pieces.darkgray.PointyHatGuysPawn)
+    assert(rook.captureSameAsMove, msg)
+  }
+
+  @Test def testPossibleCaptures(): Unit = {
+    println("possibleCaptures")
+    val forwards = new RelativePositionRange(new RelativePosition(0, 1),
+      new RelativePosition(0, 7))
+    val right = new RelativePositionRange(new RelativePosition(1, 0),
+      new RelativePosition(7, 0))
+    val backwards = new RelativePositionRange(new RelativePosition(0, -1),
+      new RelativePosition(0, -7))
+    val left = new RelativePositionRange(new RelativePosition(-1, 0),
+      new RelativePosition(-7, 0))
+    val rook = new PromotedRook(game.pieces.lightgray.EmperorsPawn)
+    val expected = Set(forwards, right, backwards, left)
+    val actual = rook.possibleCaptures
+    assertEquals(expected, actual)
   }
 
 }
