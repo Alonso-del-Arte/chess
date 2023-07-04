8a4ad967da936fa3e79f707fd72781ab57f5fdd8
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-03 23:44:40 -0400

    Rewind to failing

--- src/game/pieces/white/WhitePiece.scala
+++ src/game/pieces/white/WhitePiece.scala
@@ -1,9 +1,9 @@
 package game.pieces.white
 
-import game.{Player, White}
+import game.{Player, Neutral, White}
 import game.pieces.Piece
 
 trait WhitePiece extends Piece {
-  override val affiliation: Player = White
+  override val affiliation: Player = Neutral
 
 }
