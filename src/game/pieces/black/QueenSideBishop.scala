aacd86cb7d89cc8d8d6bba6c9a648b52e0c5d721
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-03 23:57:07 -0400

    Passing all tests so far

--- src/game/pieces/black/QueenSideBishop.scala
+++ src/game/pieces/black/QueenSideBishop.scala
@@ -1,7 +1,26 @@
 package game.pieces.black
 
+import game.{Black, Player, RelativePosition, RelativePositionRange}
 import game.pieces.Bishop
 
 object QueenSideBishop extends Bishop with BlackPiece {
+  override val affiliation: Player = Black
+  private val toNortheast =
+    new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+  private val toNorthwest =
+    new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+  private val toSouthwest =
+    new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+  private val toSoutheast =
+    new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(toNortheast, toNorthwest, toSouthwest, toSoutheast)
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = true
+  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
 
 }
--- src/game/pieces/white/QueenSideBishop.scala
+++ src/game/pieces/white/QueenSideBishop.scala
@@ -1,7 +1,26 @@
 package game.pieces.white
 
+import game.{Player, RelativePosition, RelativePositionRange, White}
 import game.pieces.Bishop
 
 object QueenSideBishop extends Bishop with WhitePiece {
+  override val affiliation: Player = White
+  private val toNortheast =
+    new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+  private val toNorthwest =
+    new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+  private val toSouthwest =
+    new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+  private val toSoutheast =
+    new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(toNortheast, toNorthwest, toSouthwest, toSoutheast)
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = true
+  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
 
 }
