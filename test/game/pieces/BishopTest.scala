fceac7a8df7113bfe535adc4d432939c01b1ff1b
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-05 01:46:07 -0400

    Failing all tests

--- test/game/pieces/BishopTest.scala
+++ test/game/pieces/BishopTest.scala
@@ -1,11 +1,27 @@
 package game.pieces
 
-import game.{Black, Neutral, Player, RelativePositionRange, White}
+import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class BishopTest {
+
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val toNortheast = new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+    val toNorthwest = new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+    val toSouthwest = new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+    val toSoutheast = new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+    val bishop = new BishopImpl
+    val expected = Set(toNorthwest, toNortheast, toSouthwest, toSoutheast)
+    val actual = bishop.possibleMoves
+    assertEquals(expected, actual)
+  }
 
   @Test def testCanJumpOver(): Unit = {
     println("canJumpOver")
@@ -18,6 +34,14 @@ class BishopTest {
     println("captureSameAsMove")
     val bishop = new BishopImpl
     assert(bishop.captureSameAsMove, "A bishop captures same as moves")
+  }
+
+  @Test def testPossibleCaptures(): Unit = {
+    println("possibleCaptures")
+    val bishop = new BishopImpl
+    val expected = bishop.possibleMoves
+    val actual = bishop.possibleCaptures
+    assertEquals(expected, actual)
   }
 
   private class BishopImpl extends Bishop {
