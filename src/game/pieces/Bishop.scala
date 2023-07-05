763d49b504f97dba892bba4e028cf01f52144793
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-05 01:47:40 -0400

    Passing all tests so far

--- src/game/pieces/Bishop.scala
+++ src/game/pieces/Bishop.scala
@@ -3,11 +3,22 @@ package game.pieces
 import game.{RelativePosition, RelativePositionRange}
 
 abstract class Bishop extends Piece {
-  override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = false
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, 0),
-      new RelativePosition(0, 16)))
+  private val toNortheast =
+    new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+  private val toNorthwest =
+    new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+  private val toSouthwest =
+    new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+  private val toSoutheast =
+    new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(toNortheast, toNorthwest, toSouthwest, toSoutheast)
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = true
+  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
 
 }
