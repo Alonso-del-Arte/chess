adaf8bca18e000a6845015aac0ff9628e06b3e14
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-05 01:35:54 -0400

    Passing all tests so far

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -4,12 +4,23 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = false
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, 0),
-      new RelativePosition(8, 0)))
+  private val toNortheast =
+    new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+  private val toNorthwest =
+    new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+  private val toSouthwest =
+    new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+  private val toSoutheast =
+    new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(toNortheast, toNorthwest, toSouthwest, toSoutheast)
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = true
+  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
   override def prior: Pawn = pawn
 
 }
