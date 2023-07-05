31ed53217d314490e02f5f42b9661a00dcba15e7
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-05 02:07:11 -0400

    Refactored

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -4,23 +4,6 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
   override val affiliation: Player = pawn.affiliation
-  private val toNortheast =
-    new RelativePositionRange(new RelativePosition(1, -1),
-      new RelativePosition(7, -7))
-  private val toNorthwest =
-    new RelativePositionRange(new RelativePosition(1, 1),
-      new RelativePosition(7, 7))
-  private val toSouthwest =
-    new RelativePositionRange(new RelativePosition(-1, 1),
-      new RelativePosition(-7, 7))
-  private val toSoutheast =
-    new RelativePositionRange(new RelativePosition(-1, -1),
-      new RelativePosition(-7, -7))
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(toNortheast, toNorthwest, toSouthwest, toSoutheast)
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = true
-  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
   override def prior: Pawn = pawn
 
 }
