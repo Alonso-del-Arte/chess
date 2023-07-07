a3a817ef93e2991f13ab43dca34afaf346a20403
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-07 16:59:10 -0400

    Refactored

--- src/game/pieces/PromotedQueen.scala
+++ src/game/pieces/PromotedQueen.scala
@@ -1,34 +1,9 @@
 package game.pieces
 
-import game.{Player, RelativePosition, RelativePositionRange}
+import game.Player
 
 class PromotedQueen(val pawn: Pawn) extends Queen with Promoted {
   override val affiliation: Player = pawn.affiliation
-  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
-    new RelativePosition(0, 7))
-  private val right = new RelativePositionRange(new RelativePosition(1, 0),
-    new RelativePosition(7, 0))
-  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
-    new RelativePosition(0, -7))
-  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
-    new RelativePosition(-7, 0))
-  private val toNortheast =
-    new RelativePositionRange(new RelativePosition(1, -1),
-      new RelativePosition(7, -7))
-  private val toNorthwest =
-    new RelativePositionRange(new RelativePosition(1, 1),
-      new RelativePosition(7, 7))
-  private val toSouthwest =
-    new RelativePositionRange(new RelativePosition(-1, 1),
-      new RelativePosition(-7, 7))
-  private val toSoutheast =
-    new RelativePositionRange(new RelativePosition(-1, -1),
-      new RelativePosition(-7, -7))
-  override val possibleMoves: Set[RelativePositionRange] = Set(forwards, right,
-    backwards, left, toNortheast, toNorthwest, toSouthwest, toSoutheast)
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = true
-  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
   override def prior: Pawn = pawn
 
 }
--- src/game/pieces/white/Queen.scala
+++ src/game/pieces/white/Queen.scala
@@ -1,33 +1,8 @@
 package game.pieces.white
 
-import game.{Player, RelativePosition, RelativePositionRange, White}
+import game.{Player, White}
 
 object Queen extends game.pieces.Queen with WhitePiece {
   override val affiliation: Player = White
-  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
-    new RelativePosition(0, 7))
-  private val right = new RelativePositionRange(new RelativePosition(1, 0),
-    new RelativePosition(7, 0))
-  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
-    new RelativePosition(0, -7))
-  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
-    new RelativePosition(-7, 0))
-  private val toNortheast =
-    new RelativePositionRange(new RelativePosition(1, -1),
-      new RelativePosition(7, -7))
-  private val toNorthwest =
-    new RelativePositionRange(new RelativePosition(1, 1),
-      new RelativePosition(7, 7))
-  private val toSouthwest =
-    new RelativePositionRange(new RelativePosition(-1, 1),
-      new RelativePosition(-7, 7))
-  private val toSoutheast =
-    new RelativePositionRange(new RelativePosition(-1, -1),
-      new RelativePosition(-7, -7))
-  override val possibleMoves: Set[RelativePositionRange] = Set(forwards, right,
-    backwards, left, toNortheast, toNorthwest, toSouthwest, toSoutheast)
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = true
-  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
 
 }
