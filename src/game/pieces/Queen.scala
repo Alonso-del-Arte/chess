4ba9c9a0e883f86be8844e346e1ffa2a74a15b30
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-07 16:55:22 -0400

    Passing all tests so far

--- src/game/pieces/Queen.scala
+++ src/game/pieces/Queen.scala
@@ -3,11 +3,30 @@ package game.pieces
 import game.{RelativePosition, RelativePositionRange}
 
 abstract class Queen extends Piece {
-  override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = false
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, 0),
-      new RelativePosition(20, 20)))
+  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
+    new RelativePosition(0, 7))
+  private val right = new RelativePositionRange(new RelativePosition(1, 0),
+    new RelativePosition(7, 0))
+  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
+    new RelativePosition(0, -7))
+  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
+    new RelativePosition(-7, 0))
+  private val toNortheast =
+    new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+  private val toNorthwest =
+    new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+  private val toSouthwest =
+    new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+  private val toSoutheast =
+    new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+  override val possibleMoves: Set[RelativePositionRange] = Set(forwards, right,
+    backwards, left, toNortheast, toNorthwest, toSouthwest, toSoutheast)
+  override val canJumpOver: Boolean = false
+  override val captureSameAsMove: Boolean = true
+  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
 
 }
