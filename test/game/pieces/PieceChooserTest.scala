b440f3c2240c98d2d170980b3b8a0dc68cf78c56
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-13 00:06:52 -0400

    Failing choosePawn( ) test

--- test/game/pieces/PieceChooserTest.scala
+++ test/game/pieces/PieceChooserTest.scala
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class PieceChooserTest {
-  private val sides: Set[Player] = Set(White, Black, DarkGray, LightGray)
+  private val allSides: Set[Player] = Set(White, Black, DarkGray, LightGray)
   private val whitePawns: Set[Pawn] = Set(game.pieces.white.QueenSideRookPawn,
-    game.pieces.white.QueenSideKnightPawn, game.pieces.white.QueenSideBishopPawn,
-    game.pieces.white.QueenPawn, game.pieces.white.KingPawn,
-    game.pieces.white.KingSideBishopPawn, game.pieces.white.KingSideKnightPawn,
-    game.pieces.white.KingSideRookPawn)
+    game.pieces.white.QueenSideKnightPawn,
+    game.pieces.white.QueenSideBishopPawn, game.pieces.white.QueenPawn,
+    game.pieces.white.KingPawn, game.pieces.white.KingSideBishopPawn,
+    game.pieces.white.KingSideKnightPawn, game.pieces.white.KingSideRookPawn)
   private val blackPawns: Set[Pawn] = Set(game.pieces.black.QueenSideRookPawn,
-    game.pieces.black.QueenSideKnightPawn, game.pieces.black.QueenSideBishopPawn,
-    game.pieces.black.QueenPawn, game.pieces.black.KingPawn,
-    game.pieces.black.KingSideBishopPawn, game.pieces.black.KingSideKnightPawn,
-    game.pieces.black.KingSideRookPawn)
+    game.pieces.black.QueenSideKnightPawn,
+    game.pieces.black.QueenSideBishopPawn, game.pieces.black.QueenPawn,
+    game.pieces.black.KingPawn, game.pieces.black.KingSideBishopPawn,
+    game.pieces.black.KingSideKnightPawn, game.pieces.black.KingSideRookPawn)
   private val darkGrayPawns: Set[Pawn] = Set(game.pieces.darkgray.EmperorsPawn,
     game.pieces.darkgray.EmpressesPawn, game.pieces.darkgray.PointyHatGuysPawn,
     game.pieces.darkgray.HorsesPawn, game.pieces.darkgray.TowersPawn)
@@ -24,12 +24,14 @@ class PieceChooserTest {
     Set(game.pieces.lightgray.EmperorsPawn, game.pieces.lightgray.EmpressesPawn,
       game.pieces.lightgray.PointyHatGuysPawn, game.pieces.lightgray.HorsesPawn,
       game.pieces.lightgray.TowersPawn)
+  private val allPawns: Set[Pawn] = whitePawns ++ blackPawns ++ darkGrayPawns ++
+    lightGrayPawns
   private val mapSidesToPawns: Map[Player, Set[Pawn]] = Map(White -> whitePawns,
     Black -> blackPawns, DarkGray -> darkGrayPawns, LightGray -> lightGrayPawns)
 
   @Test def testChoosePawnOfGivenSide(): Unit = {
     val multiplier = 12
-    mapSidesToPawns.foreach(pair => {
+    this.mapSidesToPawns.foreach(pair => {
       val iterCount = pair._2.size * multiplier
       val expected = pair._2
       val actual = {
@@ -37,6 +39,15 @@ class PieceChooserTest {
       }.toSet
       assertEquals(expected, actual)
     })
+  }
+
+  @Test def testChoosePawn(): Unit = {
+    println("choosePawn")
+    val iterCount = this.allPawns.size * 12
+    val actual = {
+      for (_ <- 1 to iterCount) yield PieceChooser.choosePawn
+    }.toSet
+    assertEquals(this.allPawns, actual)
   }
 
 }
