68297b1ad6af95eac84a9e658a4cf712f168c714
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-08 23:57:50 -0400

    Passing choose officer piece of a given side test

--- test/game/pieces/PieceChooser.scala
+++ test/game/pieces/PieceChooser.scala
@@ -26,6 +26,29 @@ object PieceChooser {
       game.pieces.lightgray.TowersPawn)
   private val mapSidesToPawns: Map[Player, List[Pawn]] = Map(White -> whitePawns,
     Black -> blackPawns, DarkGray -> darkGrayPawns, LightGray -> lightGrayPawns)
+  private val whiteOfficerPieces: List[Piece] =
+    List(game.pieces.white.QueenSideRook, game.pieces.white.QueenSideKnight,
+      game.pieces.white.QueenSideBishop, game.pieces.white.Queen,
+      game.pieces.white.King, game.pieces.white.KingSideBishop,
+      game.pieces.white.KingSideKnight, game.pieces.white.KingSideRook)
+  private val blackOfficerPieces: List[Piece] =
+    List(game.pieces.black.QueenSideRook, game.pieces.black.QueenSideKnight,
+      game.pieces.black.QueenSideBishop, game.pieces.black.Queen,
+      game.pieces.black.King, game.pieces.black.KingSideBishop,
+      game.pieces.black.KingSideKnight, game.pieces.black.KingSideRook)
+  private val darkGrayOfficerPieces: List[Piece] =
+    List(game.pieces.darkgray.Emperor, game.pieces.darkgray.Empress,
+      game.pieces.darkgray.PointyHatGuy, game.pieces.darkgray.Horse,
+      game.pieces.darkgray.Tower)
+  private val lightGrayOfficerPieces: List[Piece] =
+    List(game.pieces.lightgray.Emperor, game.pieces.lightgray.Empress,
+      game.pieces.lightgray.PointyHatGuy, game.pieces.lightgray.Horse,
+      game.pieces.lightgray.Tower)
+  private val allOfficerPieces: List[Piece] = whiteOfficerPieces ++
+    blackOfficerPieces ++ darkGrayOfficerPieces ++ lightGrayOfficerPieces
+  private val mapSidesToOfficerPieces: Map[Player, List[Piece]] =
+    Map(White -> whiteOfficerPieces, Black -> blackOfficerPieces,
+      DarkGray -> darkGrayOfficerPieces, LightGray -> lightGrayOfficerPieces)
 
   def choosePawn: Pawn = {
     val side = this.allSides(Random.nextInt(this.allSides.size))
@@ -40,8 +63,10 @@ object PieceChooser {
   // TODO: Write tests for this
   def chooseOfficerPiece: Piece = this.choosePawn
 
-  // TODO: Write tests for this
-  def chooseOfficerPiece(side: Player): Piece = this.choosePawn
+  def chooseOfficerPiece(side: Player): Piece = {
+    val list = this.mapSidesToOfficerPieces(side)
+    list(Random.nextInt(list.size))
+  }
 
   // TODO: Write tests for this
   def choosePiece: Piece = this.choosePawn
