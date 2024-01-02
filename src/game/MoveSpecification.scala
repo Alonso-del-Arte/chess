1d915b226c94379d3922bf756898d7f09d2fe316
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-02 02:00:43 -0500

    Added a parameter to the constructor

--- src/game/MoveSpecification.scala
+++ src/game/MoveSpecification.scala
@@ -2,4 +2,5 @@ package game
 
 import game.pieces.Piece
 
-class MoveSpecification(val piece: Piece, val destination: Position)
+class MoveSpecification(val piece: Piece, val start: Position,
+                        val destination: Position)
