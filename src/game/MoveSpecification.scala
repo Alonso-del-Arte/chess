11da07195b63f6f36684732ab19b814c97e76900
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-20 16:51:45 -0500

    Added optional prerequisite function parameter

--- src/game/MoveSpecification.scala
+++ src/game/MoveSpecification.scala
@@ -2,5 +2,13 @@ package game
 
 import game.pieces.Piece
 
+object MoveSpecification {
+
+  def tempFiller(state: BoardState): Boolean = true
+
+}
+
 class MoveSpecification(val piece: Piece, val start: Position,
-                        val destination: Position)
+                        val destination: Position,
+                        val meetsPrerequisites: Option[BoardState => Boolean]
+                        = Option(MoveSpecification.tempFiller))
