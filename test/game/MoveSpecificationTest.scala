b7c8f8dac37a7cb965f0aee0fc8e1966bbaac1b1
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-20 17:09:38 -0500

    Failing test that default prerequisites should be empty Option

--- test/game/MoveSpecificationTest.scala
+++ test/game/MoveSpecificationTest.scala
@@ -9,4 +9,15 @@ import scala.util.Random
 
 class MoveSpecificationTest {
 
+  @Test def testDefaultMeetsPrerequisitesIsEmptyOption(): Unit = {
+    val piece = PieceChooser.choosePiece
+    val start = new PositionImpl(Random.nextInt(8) + 1, Random.nextInt(8) + 1)
+    val offset = new RelativePosition(Random.nextInt(8), Random.nextInt(8))
+    val destination = start.translate(offset)
+    val instance = new MoveSpecification(piece, start, destination)
+    val actual = instance.prerequisites
+    val msg = "Default prerequisites should be empty Option"
+    assert(actual.isEmpty, msg)
+  }
+
 }
