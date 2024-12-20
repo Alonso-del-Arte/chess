25f5e2d3db8212a3c1a907dc378edea57089009f
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-20 17:07:13 -0500

    New test class

---
+++ test/game/MoveSpecificationTest.scala
@@ -0,0 +1,12 @@
+package game
+
+import game.pieces.{Piece, PieceChooser}
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+import scala.util.Random
+
+class MoveSpecificationTest {
+
+}
