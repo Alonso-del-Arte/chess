70aa34f372536b66607482d95e9f1df69f3fdc72
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-24 23:32:21 -0500

    Removed unused imports

--- test/game/MoveSpecificationTest.scala
+++ test/game/MoveSpecificationTest.scala
@@ -1,9 +1,8 @@
 package game
 
-import game.pieces.{Piece, PieceChooser}
+import game.pieces.PieceChooser
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions._
 
 import scala.util.Random
 
