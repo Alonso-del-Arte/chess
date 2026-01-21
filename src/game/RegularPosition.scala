9b86f085562f6f14e1dc97747e6543d1b69e5f21
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-20 23:06:46 -0500

    Passing equals( ) test

--- src/game/RegularPosition.scala
+++ src/game/RegularPosition.scala
@@ -29,8 +29,7 @@ class RegularPosition(x: Int, y: Int) extends Position(x, y) {
 
   // TODO: Write tests for this
   override def equals(obj: Any): Boolean = obj match {
-    case other: RegularPosition =>
-      System.identityHashCode(this) == System.identityHashCode(other)
+    case other: RegularPosition => (this.x == other.x && this.y == other.y)
     case _ => false
   }
 
