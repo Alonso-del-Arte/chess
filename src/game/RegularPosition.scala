f61eea810660a77e54b3a648bf0c4d45a43591cd
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-20 23:16:58 -0500

    Refactored equals( ) and hashCode( ) overrides out

--- src/game/RegularPosition.scala
+++ src/game/RegularPosition.scala
@@ -27,13 +27,6 @@ class RegularPosition(x: Int, y: Int) extends Position(x, y) {
 
   override def toString: String = s"${(x + 96).toChar}${(y + 48).toChar}"
 
-  override def equals(obj: Any): Boolean = obj match {
-    case other: RegularPosition => this.x == other.x && this.y == other.y
-    case _ => false
-  }
-
-  override def hashCode: Int = 16 * this.x + this.y
-
   override def isOffsetWithinBounds(offset: RelativePosition,
                                     board: Board): Boolean = {
     val diffX = this.x + offset.offsetX
