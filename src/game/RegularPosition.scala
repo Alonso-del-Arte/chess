c5e724107f2677fbf349bc5436853bafdd983933
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-20 21:37:04 -0500

    Passing not equals null test

--- src/game/RegularPosition.scala
+++ src/game/RegularPosition.scala
@@ -28,7 +28,7 @@ class RegularPosition(x: Int, y: Int) extends Position(x, y) {
   override def toString: String = s"${(x + 96).toChar}${(y + 48).toChar}"
 
   // TODO: Write tests for this
-  override def equals(obj: Any): Boolean = true
+  override def equals(obj: Any): Boolean = obj != null
 
   // TODO: Write tests for this
   override def hashCode: Int = 0
