ac72136a3f4b97d9e3504b600c46718683c05035
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 21:33:20 -0500

    Checkerboard pattern for getColor( )

--- test/game/BoardImpl.scala
+++ test/game/BoardImpl.scala
@@ -6,6 +6,12 @@ class BoardImpl(maxX: Int, maxY: Int) extends Board(maxX, maxY) {
 
   override def contains(position: Position): Boolean = false
 
-  override def getColor(position: Position): Color = Color.BLACK
+  override def getColor(position: Position): Color = {
+    if ((position.x + position.y) % 2 == 0) {
+      Color.BLACK
+    } else {
+      Color.WHITE
+    }
+  }
 
 }
