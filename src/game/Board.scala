0f1e4503f454d660a362e518c3dc9a2024a00007
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 23:17:50 -0500

    Passing does not contain too far down test

--- src/game/Board.scala
+++ src/game/Board.scala
@@ -5,7 +5,7 @@ import java.awt.Color
 abstract class Board(val maxX: Int, val maxY: Int, val minX: Int = 1,
                      val minY: Int = 1) {
 
-  def contains(position: Position): Boolean = true
+  def contains(position: Position): Boolean = position.y >= minY
 
   def getColor(position: Position): Color
 
