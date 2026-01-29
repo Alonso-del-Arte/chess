fcfef408325e7962a2b7f51843329b3f1c74e16d
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 23:25:23 -0500

    Passing does not contain too far up test

--- src/game/Board.scala
+++ src/game/Board.scala
@@ -6,7 +6,7 @@ abstract class Board(val maxX: Int, val maxY: Int, val minX: Int = 1,
                      val minY: Int = 1) {
 
   def contains(position: Position): Boolean =
-    (position.x >= minX) && (position.y >= minY)
+    (position.x >= minX) && (position.y >= minY) && (position.y <= maxY)
 
   def getColor(position: Position): Color
 
