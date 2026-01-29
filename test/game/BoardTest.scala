0fa415d7dd8abc84b67b922740c1910bd6c632f2
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 23:04:21 -0500

    Failing contains( ) test

--- test/game/BoardTest.scala
+++ test/game/BoardTest.scala
@@ -7,4 +7,16 @@ import org.junit.jupiter.api.Assertions._
 
 class BoardTest {
 
+  @Test def testContains(): Unit = {
+    println("contains")
+    val maxX = Random.nextInt(24) + 16
+    val maxY = Random.nextInt(24) + 16
+    val board = new BoardImpl(maxX, maxY)
+    val x = Random.nextInt(maxX) + 1
+    val y = Random.nextInt(maxY) + 1
+    val position = new PositionImpl(x, y)
+    val msg = s"$maxX by $maxY board should contain $position"
+    assert(board.contains(position), msg)
+  }
+
 }
