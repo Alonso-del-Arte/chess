24914e1b13c47b47a45a9d8f12c29f629b5ee94e
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 23:21:30 -0500

    Passing does not contain too far left test

--- src/game/Board.scala
+++ src/game/Board.scala
@@ -5,7 +5,8 @@ import java.awt.Color
 abstract class Board(val maxX: Int, val maxY: Int, val minX: Int = 1,
                      val minY: Int = 1) {
 
-  def contains(position: Position): Boolean = position.y >= minY
+  def contains(position: Position): Boolean =
+    (position.x >= minX) && (position.y >= minY)
 
   def getColor(position: Position): Color
 
