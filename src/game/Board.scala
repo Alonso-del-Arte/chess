ec1dcef9347137723e60131a0fc447eb2b3ebefa
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 21:35:34 -0500

    Changed contains( ) to concrete stub

--- src/game/Board.scala
+++ src/game/Board.scala
@@ -5,7 +5,7 @@ import java.awt.Color
 abstract class Board(val maxX: Int, val maxY: Int, val minX: Int = 1,
                      val minY: Int = 1) {
 
-  def contains(position: Position): Boolean
+  def contains(position: Position): Boolean = false
 
   def getColor(position: Position): Color
 
