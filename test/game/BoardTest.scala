f38767678d932886d5d1042d76ca6a564dea9f3e
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 23:15:40 -0500

    Failing does not contain too far down test

--- test/game/BoardTest.scala
+++ test/game/BoardTest.scala
@@ -19,4 +19,15 @@ class BoardTest {
     assert(board.contains(position), msg)
   }
 
+  @Test def testDoesNotContainTooFarDown(): Unit = {
+    val maxX = Random.nextInt(24) + 16
+    val maxY = Random.nextInt(24) + 16
+    val board = new BoardImpl(maxX, maxY)
+    val x = Random.nextInt(maxX) + 1
+    val y = -Random.nextInt(maxY)
+    val position = new PositionImpl(x, y)
+    val msg = s"$maxX by $maxY board should not contain $position"
+    assert(!board.contains(position), msg)
+  }
+
 }
