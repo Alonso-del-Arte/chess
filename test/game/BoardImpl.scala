e723cc99eb25647044cd9c086834afd169431ce5
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-28 21:36:37 -0500

    Removed contains( ) override

--- test/game/BoardImpl.scala
+++ test/game/BoardImpl.scala
@@ -4,8 +4,6 @@ import java.awt.Color
 
 class BoardImpl(maxX: Int, maxY: Int) extends Board(maxX, maxY) {
 
-  override def contains(position: Position): Boolean = false
-
   override def getColor(position: Position): Color = {
     if ((position.x + position.y) % 2 == 0) {
       Color.BLACK
