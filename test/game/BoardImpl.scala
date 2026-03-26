2092cfffe5344f0a249db3869f50777ade8ddd25
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-26 18:18:23 -0400

    Removed getColor( ) override

--- test/game/BoardImpl.scala
+++ test/game/BoardImpl.scala
@@ -1,15 +1,3 @@
 package game
 
-import java.awt.Color
-
-class BoardImpl(maxX: Int, maxY: Int) extends Board(maxX, maxY) {
-
-  override def getColor(position: Position): Color = {
-    if ((position.x + position.y) % 2 == 0) {
-      Color.BLACK
-    } else {
-      Color.WHITE
-    }
-  }
-
-}
+class BoardImpl(maxX: Int, maxY: Int) extends Board(maxX, maxY)
