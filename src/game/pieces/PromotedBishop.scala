f343c508e5b67b35afbbc61f9755ae1199114843
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-20 23:41:29 -0400

    Back to passing capture same as move test

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -4,7 +4,7 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val captureSameAsMove: Boolean = false
+  override val captureSameAsMove: Boolean = true
   override def prior: Pawn = new Pawn {
     override val affiliation: Player = Neutral
     override val possibleMoves: Set[RelativePositionRange] = Set()
