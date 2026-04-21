632e49d3099484a2757db8e0b58ceddba28f722e
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-20 23:44:11 -0400

    Preparing to fail the next test

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -4,6 +4,7 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
   override val affiliation: Player = pawn.affiliation
+  override val possibleCaptures: Set[RelativePositionRange] = Set()
   override def prior: Pawn = new Pawn {
     override val affiliation: Player = Neutral
     override val possibleMoves: Set[RelativePositionRange] = Set()
