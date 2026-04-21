ce30ca6205852792ac3b45e213170703cbee9dba
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-20 23:33:31 -0400

    Back to passing canJumpOver( ) test

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -4,7 +4,7 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val canJumpOver: Boolean = true
+  override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = false
   override def prior: Pawn = new Pawn {
     override val affiliation: Player = Neutral
