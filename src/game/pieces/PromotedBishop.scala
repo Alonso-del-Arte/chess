7f2240ec21bea53d561511563a16449dda6d09d8
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-20 23:31:21 -0400

    Refactored possibleMoves( ) by deleting override

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -6,16 +6,6 @@ class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
   override val affiliation: Player = pawn.affiliation
   override val canJumpOver: Boolean = true
   override val captureSameAsMove: Boolean = false
-  val toNortheast = new RelativePositionRange(new RelativePosition(1, -1),
-    new RelativePosition(7, -7))
-  val toNorthwest = new RelativePositionRange(new RelativePosition(1, 1),
-    new RelativePosition(7, 7))
-  val toSouthwest = new RelativePositionRange(new RelativePosition(-1, 1),
-    new RelativePosition(-7, 7))
-  val toSoutheast = new RelativePositionRange(new RelativePosition(-1, -1),
-    new RelativePosition(-7, -7))
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(toNorthwest, toNortheast, toSouthwest, toSoutheast)
   override def prior: Pawn = new Pawn {
     override val affiliation: Player = Neutral
     override val possibleMoves: Set[RelativePositionRange] = Set()
