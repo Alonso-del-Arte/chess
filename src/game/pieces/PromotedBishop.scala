98eff9032f5166d393acb21e55360ed108161ad4
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-20 23:17:23 -0400

    Partial rewind to failing

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -1,9 +1,14 @@
 package game.pieces
 
-import game.{Player, RelativePosition, RelativePositionRange}
+import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
-  override val affiliation: Player = pawn.affiliation
-  override def prior: Pawn = pawn
+  override val affiliation: Player = game.Neutral//pawn.affiliation
+  override val canJumpOver: Boolean = true
+  override val captureSameAsMove: Boolean = false
+  override def prior: Pawn = new Pawn {
+    override val affiliation: Player = Neutral
+    override val possibleMoves: Set[RelativePositionRange] = Set()
+  }//pawn
 
 }
