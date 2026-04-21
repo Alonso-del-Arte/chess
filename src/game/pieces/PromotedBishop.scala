2035780405e32c681c4ec759e906d5536ba71018
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-20 23:27:25 -0400

    Preparing to fail the next test

--- src/game/pieces/PromotedBishop.scala
+++ src/game/pieces/PromotedBishop.scala
@@ -6,6 +6,7 @@ class PromotedBishop(val pawn: Pawn) extends Bishop with Promoted {
   override val affiliation: Player = pawn.affiliation
   override val canJumpOver: Boolean = true
   override val captureSameAsMove: Boolean = false
+  override val possibleMoves: Set[RelativePositionRange] = Set()
   override def prior: Pawn = new Pawn {
     override val affiliation: Player = Neutral
     override val possibleMoves: Set[RelativePositionRange] = Set()
