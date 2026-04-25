be75194cdcc07ffc07fe24966e4e45dad0004030
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-24 23:31:18 -0400

    Refactored possible moves by deleting override

--- src/game/pieces/PromotedQueen.scala
+++ src/game/pieces/PromotedQueen.scala
@@ -4,28 +4,6 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedQueen(val pawn: Pawn) extends Queen with Promoted {
   override val affiliation: Player = pawn.affiliation
-  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
-    new RelativePosition(0, 7))
-  private val right = new RelativePositionRange(new RelativePosition(1, 0),
-    new RelativePosition(7, 0))
-  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
-    new RelativePosition(0, -7))
-  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
-    new RelativePosition(-7, 0))
-  private val toNortheast =
-    new RelativePositionRange(new RelativePosition(1, -1),
-      new RelativePosition(7, -7))
-  private val toNorthwest =
-    new RelativePositionRange(new RelativePosition(1, 1),
-      new RelativePosition(7, 7))
-  private val toSouthwest =
-    new RelativePositionRange(new RelativePosition(-1, 1),
-      new RelativePosition(-7, 7))
-  private val toSoutheast =
-    new RelativePositionRange(new RelativePosition(-1, -1),
-      new RelativePosition(-7, -7))
-  override val possibleMoves: Set[RelativePositionRange] = Set(forwards, right,
-    backwards, left, toNortheast, toNorthwest, toSouthwest, toSoutheast)
   override def prior: Pawn = pawn
 
 }
