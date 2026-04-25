3bb662d980939b74e5a18d32ab43751aeb3a18a7
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-24 23:49:55 -0400

    Passing possible moves test

--- test/game/pieces/NeutralPawn.scala
+++ test/game/pieces/NeutralPawn.scala
@@ -3,8 +3,7 @@ package game.pieces
 import game.{Player, RelativePosition, RelativePositionRange}
 
 object NeutralPawn extends Pawn {
-  override val affiliation: Player = game.Black
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(0, 1)))
+  override val affiliation: Player = game.White
+  override val possibleMoves: Set[RelativePositionRange] = Set()
 
 }
