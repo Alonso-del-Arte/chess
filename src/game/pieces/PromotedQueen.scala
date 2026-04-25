372c6a8bd1b4df7aa3b59d3a4f82871b5d9804c7
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-24 23:57:41 -0400

    Preparing to fail queen can't jump over test

--- src/game/pieces/PromotedQueen.scala
+++ src/game/pieces/PromotedQueen.scala
@@ -4,6 +4,7 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedQueen(val pawn: Pawn) extends Queen with Promoted {
   override val affiliation: Player = pawn.affiliation
+  override val canJumpOver: Boolean = true
   override def prior: Pawn = pawn
 
 }
