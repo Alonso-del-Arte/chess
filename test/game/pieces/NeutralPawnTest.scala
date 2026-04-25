efafd9d964afcca169ceb87da8bcdddb96a1cb05
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-24 23:47:10 -0400

    New test class

---
+++ test/game/pieces/NeutralPawnTest.scala
@@ -0,0 +1,10 @@
+package game.pieces
+
+import game.{Black, RelativePosition, RelativePositionRange, White}
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class NeutralPawnTest {
+
+}
