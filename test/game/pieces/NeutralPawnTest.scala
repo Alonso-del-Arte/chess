6dfcc3ca5fc8547cc88e5bb7d60615e42ab5093b
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-24 23:54:00 -0400

    Failing affiliation test

--- test/game/pieces/NeutralPawnTest.scala
+++ test/game/pieces/NeutralPawnTest.scala
@@ -1,13 +1,21 @@
 package game.pieces
 
-import game.{Black, RelativePosition, RelativePositionRange, White}
+import game.{Black, Neutral, RelativePosition, RelativePositionRange, White}
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
 
 class NeutralPawnTest {
 
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    val expected = Neutral
+    val actual = NeutralPawn.affiliation
+    assertEquals(expected, actual)
+  }
+
   @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
     val expected: Set[RelativePositionRange] = Set()
     val actual = NeutralPawn.possibleMoves
     assertEquals(expected, actual)
