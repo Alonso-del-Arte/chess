f29f14dd4b9082960fa8fed85a4d4ac8bf313508
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-24 22:35:12 -0400

    Passing captures same as moves test

--- src/game/pieces/Knight.scala
+++ src/game/pieces/Knight.scala
@@ -5,7 +5,7 @@ import game.{RelativePosition, RelativePositionRange}
 abstract class Knight extends Piece {
   override val possibleMoves: Set[RelativePositionRange] = Set()
   override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = false
+  override val captureSameAsMove: Boolean = true
   override val possibleCaptures: Set[RelativePositionRange] =
     Set(new RelativePositionRange(new RelativePosition(0, 0),
       new RelativePosition(8, 8)))
