9be23dd7b294350922741571d4e096a6e70cb037
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-28 22:41:06 -0400

    Preparing to fail possible captures test

--- src/game/pieces/PromotedQueen.scala
+++ src/game/pieces/PromotedQueen.scala
@@ -4,6 +4,7 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedQueen(val pawn: Pawn) extends Queen with Promoted {
   override val affiliation: Player = pawn.affiliation
+  override val possibleCaptures: Set[RelativePositionRange] = Set()
   override def prior: Pawn = pawn
 
 }
