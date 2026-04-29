fbca7c07c521d1cf6035aaf4600a24a37ab4cdbe
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-29 01:12:17 -0400

    Back to passing possible moves test

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,7 +4,16 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val possibleMoves: Set[RelativePositionRange] = Set()
+  val forwards = new RelativePositionRange(new RelativePosition(0, 1),
+    new RelativePosition(0, 7))
+  val right = new RelativePositionRange(new RelativePosition(1, 0),
+    new RelativePosition(7, 0))
+  val backwards = new RelativePositionRange(new RelativePosition(0, -1),
+    new RelativePosition(0, -7))
+  val left = new RelativePositionRange(new RelativePosition(-1, 0),
+    new RelativePosition(-7, 0))
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(forwards, right, backwards, left)
   override def prior: Pawn = pawn
 
 }
