aabf458a0aecd156c0a90a18296466b8da39e597
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-29 01:12:55 -0400

    Refactored possible moves by deleting override

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,16 +4,6 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  val forwards = new RelativePositionRange(new RelativePosition(0, 1),
-    new RelativePosition(0, 7))
-  val right = new RelativePositionRange(new RelativePosition(1, 0),
-    new RelativePosition(7, 0))
-  val backwards = new RelativePositionRange(new RelativePosition(0, -1),
-    new RelativePosition(0, -7))
-  val left = new RelativePositionRange(new RelativePosition(-1, 0),
-    new RelativePosition(-7, 0))
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(forwards, right, backwards, left)
   override def prior: Pawn = pawn
 
 }
