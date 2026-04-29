8f5f951bcc5709f556a94423b821bea0f8e8764f
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-28 22:36:27 -0400

    Preparing to fail capture same as move test

--- src/game/pieces/PromotedQueen.scala
+++ src/game/pieces/PromotedQueen.scala
@@ -4,6 +4,7 @@ import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedQueen(val pawn: Pawn) extends Queen with Promoted {
   override val affiliation: Player = pawn.affiliation
+  override val captureSameAsMove: Boolean = false
   override def prior: Pawn = pawn
 
 }
