24e4982faaef302f5934de5f501204f0beb7cb2f
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-29 00:45:20 -0400

    Passing prior( ) test

--- src/game/pieces/PromotedQueen.scala
+++ src/game/pieces/PromotedQueen.scala
@@ -4,9 +4,6 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedQueen(val pawn: Pawn) extends Queen with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override def prior: Pawn = new Pawn {
-    override val affiliation: Player = Neutral
-    override val possibleMoves: Set[RelativePositionRange] = Set()
-  }
+  override def prior: Pawn = pawn
 
 }
