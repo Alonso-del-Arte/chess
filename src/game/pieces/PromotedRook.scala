c72054a276851fa8ea69eaec0b4a63b0f4ca91ab
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-30 00:20:14 -0400

    Refactored possible captures by deleting override

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -1,10 +1,9 @@
 package game.pieces
 
-import game.{Neutral, Player, RelativePosition, RelativePositionRange}
+import game.{Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
   override def prior: Pawn = pawn
 
 }
