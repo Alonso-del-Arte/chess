5cf270ff0f0126f467956b7ed309ca7a14f37f2f
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-30 00:11:19 -0400

    Refactored capture same as move by deleting override

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,7 +4,6 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val captureSameAsMove: Boolean = true
   override def prior: Pawn = pawn
 
 }
