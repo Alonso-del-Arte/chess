0344a6d97e6ca3797db8659c9142ab12f56e80b1
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-29 23:59:06 -0400

    Refactored can jump over by deleting override

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,7 +4,6 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val canJumpOver: Boolean = false
   override def prior: Pawn = pawn
 
 }
