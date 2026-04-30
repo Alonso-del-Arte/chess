24116285119740d7e9d2ac3e4eeeb5430be36a8d
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-29 23:58:25 -0400

    Back to passing can jump over test

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,7 +4,7 @@ import game.{Neutral, Player, RelativePosition, RelativePositionRange}
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val canJumpOver: Boolean = true
+  override val canJumpOver: Boolean = false
   override def prior: Pawn = pawn
 
 }
