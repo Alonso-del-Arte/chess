62af6ec3a7b77d5e1a522cb1f946dc6ffeaefd58
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-04 22:35:08 -0400

    Failing translate( ) test

--- test/game/PositionImplTest.scala
+++ test/game/PositionImplTest.scala
@@ -7,4 +7,18 @@ import org.junit.jupiter.api.Assertions._
 
 class PositionImplTest {
 
+  @Test def testTranslate(): Unit = {
+    println("translate")
+    val x = Random.nextInt(8) + 1
+    val y = Random.nextInt(8) + 1
+    val instance = new PositionImpl(x, y)
+    val offsetX = Random.nextInt(8) - 4
+    val offsetY = Random.nextInt(8) - 4
+    val offset = new RelativePosition(offsetX, offsetY)
+    val expected = new PositionImpl(x + offsetX, y + offsetY)
+    val actual = instance.translate(offset)
+    val message = s"Translating $instance by $offset"
+    assertEquals(expected, actual, message)
+  }
+
 }
