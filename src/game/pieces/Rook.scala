d5f7d78ec41a39edd09ecd850829e997c1fb2fec
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-07 01:35:29 -0400

    Refactored possible moves

--- src/game/pieces/Rook.scala
+++ src/game/pieces/Rook.scala
@@ -26,16 +26,7 @@ object Rook {
 }
 
 abstract class Rook extends Piece {
-  private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
-    new RelativePosition(0, 7))
-  private val right = new RelativePositionRange(new RelativePosition(1, 0),
-    new RelativePosition(7, 0))
-  private val backwards = new RelativePositionRange(new RelativePosition(0, -1),
-    new RelativePosition(0, -7))
-  private val left = new RelativePositionRange(new RelativePosition(-1, 0),
-    new RelativePosition(-7, 0))
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(forwards, right, backwards, left)
+  override val possibleMoves: Set[RelativePositionRange] = Rook.moves
   override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = true
   override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
