9201bed6aefb4b41c6fe8936667393e5b876c4af
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-07 00:50:01 -0400

    Refactored possible moves

--- src/game/pieces/Bishop.scala
+++ src/game/pieces/Bishop.scala
@@ -26,20 +26,7 @@ object Bishop {
 }
 
 abstract class Bishop extends Piece {
-  private val toNortheast =
-    new RelativePositionRange(new RelativePosition(1, -1),
-      new RelativePosition(7, -7))
-  private val toNorthwest =
-    new RelativePositionRange(new RelativePosition(1, 1),
-      new RelativePosition(7, 7))
-  private val toSouthwest =
-    new RelativePositionRange(new RelativePosition(-1, 1),
-      new RelativePosition(-7, 7))
-  private val toSoutheast =
-    new RelativePositionRange(new RelativePosition(-1, -1),
-      new RelativePosition(-7, -7))
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(toNortheast, toNorthwest, toSouthwest, toSoutheast)
+  override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
   override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = true
   override val possibleCaptures: Set[RelativePositionRange] = this.possibleMoves
