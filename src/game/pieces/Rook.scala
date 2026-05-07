efa0c29db5ad22a6b4164cb70eb98169c129e80d
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-07 01:04:01 -0400

    Companion object stub

--- src/game/pieces/Rook.scala
+++ src/game/pieces/Rook.scala
@@ -2,6 +2,33 @@ package game.pieces
 
 import game.{RelativePosition, RelativePositionRange}
 
+object Rook {
+
+  // TODO: Write test for this
+  val moveForward: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+
+  // TODO: Write test for this
+  val moveRight: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+
+  // TODO: Write test for this
+  val moveBack: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+
+  // TODO: Write test for this
+  val moveLeft: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+
+  // TODO: Write test for this
+  val moves: Set[RelativePositionRange] = Set()
+
+}
+
 abstract class Rook extends Piece {
   private val forwards = new RelativePositionRange(new RelativePosition(0, 1),
     new RelativePosition(0, 7))
