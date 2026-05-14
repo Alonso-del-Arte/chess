90b2dcd40ea2fbc4b8a845919e0e9442f702d19b
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-14 00:37:11 -0400

    Changed possibleCaptures stub to not duplicated superclass's stub

--- src/game/pieces/PromotedKnight.scala
+++ src/game/pieces/PromotedKnight.scala
@@ -7,7 +7,7 @@ class PromotedKnight(val pawn: Pawn) extends Knight with Promoted {
   override val possibleMoves: Set[RelativePositionRange] = Set()
   override val possibleCaptures: Set[RelativePositionRange] =
     Set(new RelativePositionRange(new RelativePosition(0, 0),
-      new RelativePosition(8, 8)))
+      new RelativePosition(9, 8)))
   override def prior: Pawn = pawn
 
 }
