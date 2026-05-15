fb4096549f899747ac97276b71fa8027c413a3a9
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-15 01:09:48 -0400

    Refactored hasSpecialMoves by deleting override

--- src/game/pieces/Rook.scala
+++ src/game/pieces/Rook.scala
@@ -29,6 +29,5 @@ abstract class Rook extends Piece {
   override val possibleMoves: Set[RelativePositionRange] = Rook.moves
   override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = true
-  override val hasSpecialMoves: Boolean = false
 
 }
