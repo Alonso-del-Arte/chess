94a8393415bf4480647b49128ae7aa5ac35f27f5
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-15 01:00:30 -0400

    Passing hasSpecialMoves test

--- src/game/pieces/Piece.scala
+++ src/game/pieces/Piece.scala
@@ -7,8 +7,7 @@ abstract class Piece {
   val possibleMoves: Set[RelativePositionRange]
   val canJumpOver: Boolean = false
   val captureSameAsMove: Boolean = true
-  // TODO: Write a test for this
-  val hasSpecialMoves: Boolean = true
+  val hasSpecialMoves: Boolean = false
 
   def possibleCaptures: Set[RelativePositionRange] =
     if (this.captureSameAsMove) this.possibleMoves else Set()
