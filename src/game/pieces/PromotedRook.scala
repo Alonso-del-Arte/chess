af8d461a694884fc19804554e6fd70a1dce0fbbc
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-15 23:12:36 -0400

    Passing couldCastle( ) test

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,6 +4,7 @@ import game.Player
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
+  override val couldCastle: Boolean = false
   override def prior: Pawn = pawn
 
 }
