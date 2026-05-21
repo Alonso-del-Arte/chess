ff77b469980f53a39fbc9aaf07d2e417b9b718f8
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-20 23:45:29 -0400

    Refactored has special moves by deleting override

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,7 +4,6 @@ import game.Player
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val hasSpecialMoves: Boolean = false
   override val couldCastle: Boolean = false
   override def prior: Pawn = pawn
 
