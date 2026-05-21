cd391e91e5fd56fa62c26d216bd4d555091db5a5
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-20 23:42:46 -0400

    Preparing to fail the next test

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,6 +4,8 @@ import game.Player
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
+  // TODO: Write a test for this
+  override val hasSpecialMoves: Boolean = true
   override val couldCastle: Boolean = false
   override def prior: Pawn = pawn
 
