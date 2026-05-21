50e45649ac08fc1a3d8dfeef8250d9e2ba1c4ca5
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-20 23:44:05 -0400

    Passing has special moves test

--- src/game/pieces/PromotedRook.scala
+++ src/game/pieces/PromotedRook.scala
@@ -4,8 +4,7 @@ import game.Player
 
 class PromotedRook(val pawn: Pawn) extends Rook with Promoted {
   override val affiliation: Player = pawn.affiliation
-  // TODO: Write a test for this
-  override val hasSpecialMoves: Boolean = true
+  override val hasSpecialMoves: Boolean = false
   override val couldCastle: Boolean = false
   override def prior: Pawn = pawn
 
