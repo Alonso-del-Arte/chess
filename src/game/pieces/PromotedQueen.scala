57b0901bc7dc5d7952ddd2e978b923741eb55637
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-26 17:20:58 -0400

    Refactored hasSpecialMoves by deleting override

--- src/game/pieces/PromotedQueen.scala
+++ src/game/pieces/PromotedQueen.scala
@@ -5,6 +5,5 @@ import game.Player
 class PromotedQueen(val pawn: Pawn) extends Queen with Promoted {
   override val affiliation: Player = pawn.affiliation
   override def prior: Pawn = pawn
-  override val hasSpecialMoves: Boolean = false
 
 }
