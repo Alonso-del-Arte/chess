7421c9eee2a12f7d4146f83db05409af546aad5a
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 01:18:38 -0400

    New test class

---
+++ test/game/pieces/black/KingTest.scala
@@ -0,0 +1,10 @@
+package game.pieces.black
+
+import game.{Black, RelativePosition, RelativePositionRange}
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class KingTest {
+
+}
