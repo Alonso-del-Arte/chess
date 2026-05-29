4d8628538efa41a35703da8325662294f1e42dc0
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 01:24:05 -0400

    Preparing to fail the next few tests

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -1,5 +1,12 @@
 package game.pieces.black
 
+import game.{Neutral, Player, RelativePositionRange}
+
 object King extends game.pieces.King with BlackPiece {
+  override val affiliation: Player = Neutral
+  override val possibleMoves: Set[RelativePositionRange] = Set()
+  override val canJumpOver: Boolean = true
+  override val captureSameAsMove: Boolean = false
+  override val hasSpecialMoves: Boolean = false
 
 }
