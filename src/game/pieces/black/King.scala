0ee5edbc0da56d795f456f6581e6a5b44dbbe8a8
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 01:54:50 -0400

    Preparing to fail tests pertaining to moves, captures

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -1,10 +1,14 @@
 package game.pieces.black
 
-import game.{Black, Player, RelativePositionRange}
+import game.{Black, Player, RelativePosition, RelativePositionRange}
 
 object King extends game.pieces.King with BlackPiece {
   // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
   override val captureSameAsMove: Boolean = true
+  // TODO: Write a test for this
+  override def possibleCaptures: Set[RelativePositionRange] =
+    Set(new RelativePositionRange(new RelativePosition(0, 1),
+      new RelativePosition(5, 9)))
 
 }
