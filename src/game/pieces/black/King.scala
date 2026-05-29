67397b6a205edc9cbf3118727a76cbfcf2f2165c
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 01:35:49 -0400

    Passing can jump over test

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -3,8 +3,9 @@ package game.pieces.black
 import game.{Black, Player, RelativePositionRange}
 
 object King extends game.pieces.King with BlackPiece {
+  // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val canJumpOver: Boolean = true
+  override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = false
   override val hasSpecialMoves: Boolean = false
 
