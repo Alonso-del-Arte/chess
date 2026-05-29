fa694388d54c3295ebd0b40b7c381455484900e8
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 01:45:05 -0400

    Passing capture same as move test

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -5,7 +5,7 @@ import game.{Black, Player, RelativePositionRange}
 object King extends game.pieces.King with BlackPiece {
   // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val captureSameAsMove: Boolean = false
+  override val captureSameAsMove: Boolean = true
   override val hasSpecialMoves: Boolean = false
 
 }
