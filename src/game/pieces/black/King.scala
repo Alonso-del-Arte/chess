a3f2408d505ed04330e78c054ba6dd5288a06f0b
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 01:50:29 -0400

    Refactored has special moves by deleting override

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -6,6 +6,5 @@ object King extends game.pieces.King with BlackPiece {
   // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
   override val captureSameAsMove: Boolean = true
-  override val hasSpecialMoves: Boolean = true
 
 }
