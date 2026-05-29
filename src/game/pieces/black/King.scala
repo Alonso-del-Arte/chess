86dc4f8c16d31b6a7ca6047c894f604e24265189
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 01:27:54 -0400

    Refactored affiliation by deleting override

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -3,7 +3,6 @@ package game.pieces.black
 import game.{Black, Player, RelativePositionRange}
 
 object King extends game.pieces.King with BlackPiece {
-  override val affiliation: Player = Black
   override val possibleMoves: Set[RelativePositionRange] = Set()
   override val canJumpOver: Boolean = true
   override val captureSameAsMove: Boolean = false
