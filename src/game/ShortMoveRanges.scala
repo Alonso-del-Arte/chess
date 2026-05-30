ed0f1e64bbb1599d9fc5314432aa6268d4030e2f
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 23:25:45 -0400

    Passing moveNorthwest test

--- src/game/ShortMoveRanges.scala
+++ src/game/ShortMoveRanges.scala
@@ -6,10 +6,9 @@ object ShortMoveRanges {
     new RelativePositionRange(new RelativePosition(1, -1),
       new RelativePosition(1, -1))
 
-  // TODO: Write a test for this
   val moveNorthwest: RelativePositionRange =
     new RelativePositionRange(new RelativePosition(1, 1),
-      new RelativePosition(7, 7))
+      new RelativePosition(1, 1))
 
   // TODO: Write a test for this
   val moveSouthwest: RelativePositionRange =
