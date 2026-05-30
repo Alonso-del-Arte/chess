25042d1aff3a3be9c1324dc92cb2d7cd82d3160f
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-29 23:18:01 -0400

    Stubs

--- src/game/ShortMoveRanges.scala
+++ src/game/ShortMoveRanges.scala
@@ -2,4 +2,44 @@ package game
 
 object ShortMoveRanges {
 
+  // TODO: Write a test for this
+  val moveNortheast: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(1, -1),
+      new RelativePosition(7, -7))
+
+  // TODO: Write a test for this
+  val moveNorthwest: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(1, 1),
+      new RelativePosition(7, 7))
+
+  // TODO: Write a test for this
+  val moveSouthwest: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(-1, 1),
+      new RelativePosition(-7, 7))
+
+  // TODO: Write a test for this
+  val moveSoutheast: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(-1, -1),
+      new RelativePosition(-7, -7))
+
+  // TODO: Write a test for this
+  val moveForward: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(0, 1),
+      new RelativePosition(0, 7))
+
+  // TODO: Write a test for this
+  val moveRight: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(1, 0),
+      new RelativePosition(7, 0))
+
+  // TODO: Write a test for this
+  val moveBack: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(0, -1),
+      new RelativePosition(0, -7))
+
+  // TODO: Write a test for this
+  val moveLeft: RelativePositionRange =
+    new RelativePositionRange(new RelativePosition(-1, 0),
+      new RelativePosition(-7, 0))
+
 }
