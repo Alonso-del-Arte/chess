5f5d40b9000658fc007de9e00852b9f3502f5b57
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-04 15:55:27 -0400

    Preparing to fail the next few tests

--- src/game/pieces/white/King.scala
+++ src/game/pieces/white/King.scala
@@ -1,5 +1,16 @@
 package game.pieces.white
 
+import game.{RelativePositionRange, ShortMoveRanges}
+
 object King extends game.pieces.King with WhitePiece {
+  // TODO: Write tests for this
+  override val possibleMoves: Set[RelativePositionRange] =
+    Set(ShortMoveRanges.moveForward)
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = false
 
 }
