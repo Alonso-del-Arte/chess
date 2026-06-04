8a326974fae6ef29ea5a2b00ada51ece479c03c1
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-04 16:08:57 -0400

    Failing affiliation test

--- test/game/pieces/white/KingTest.scala
+++ test/game/pieces/white/KingTest.scala
@@ -1,14 +1,17 @@
 package game.pieces.white
 
-import game.{Black, ShortMoveRanges}
+import game.{ShortMoveRanges, White}
 
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class KingTest {
 
-  @Test def placeholder(): Unit = {
-    println("PLACEHOLDER")
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    val expected = White
+    val actual = King.affiliation
+    assertEquals(expected, actual)
   }
 
 }
