e2bf367a6ebc35bc9c706a0465310a89f9c08a4b
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-04 15:40:42 -0400

    Refactored possibleMoves by deleting override

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -3,11 +3,6 @@ package game.pieces.black
 import game.{RelativePosition, RelativePositionRange, ShortMoveRanges}
 
 object King extends game.pieces.King with BlackPiece {
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(ShortMoveRanges.moveForward, ShortMoveRanges.moveRight,
-    ShortMoveRanges.moveBack, ShortMoveRanges.moveLeft,
-    ShortMoveRanges.moveNortheast, ShortMoveRanges.moveNorthwest,
-    ShortMoveRanges.moveSouthwest, ShortMoveRanges.moveSoutheast)
   override val captureSameAsMove: Boolean = true
   // TODO: Write a test for this
   override def possibleCaptures: Set[RelativePositionRange] =
