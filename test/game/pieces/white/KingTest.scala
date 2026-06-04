2bc6149042de12bc71b673f6e5cb81909f04677e
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-04 15:57:38 -0400

    New test class

---
+++ test/game/pieces/white/KingTest.scala
@@ -0,0 +1,14 @@
+package game.pieces.white
+
+import game.{Black, ShortMoveRanges}
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class KingTest {
+
+  @Test def placeholder(): Unit = {
+    println("PLACEHOLDER")
+  }
+
+}
