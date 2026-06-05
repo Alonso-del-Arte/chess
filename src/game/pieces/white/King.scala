abb5de51f28e3aae964321bf0ebb9423904835b7
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-04 23:52:45 -0400

    Refactored possibleMoves by deleting override

--- src/game/pieces/white/King.scala
+++ src/game/pieces/white/King.scala
@@ -3,11 +3,6 @@ package game.pieces.white
 import game.{RelativePositionRange, ShortMoveRanges}
 
 object King extends game.pieces.King with WhitePiece {
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(ShortMoveRanges.moveForward, ShortMoveRanges.moveRight,
-      ShortMoveRanges.moveBack, ShortMoveRanges.moveLeft,
-      ShortMoveRanges.moveNortheast, ShortMoveRanges.moveNorthwest,
-      ShortMoveRanges.moveSouthwest, ShortMoveRanges.moveSoutheast)
   // TODO: Write tests for this
   override val canJumpOver: Boolean = true
   // TODO: Write tests for this
