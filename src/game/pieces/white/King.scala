2c6947d406d9f0f03349033fef2c2bbde5e646a4
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-04 23:51:18 -0400

    Passing possibleMoves test

--- src/game/pieces/white/King.scala
+++ src/game/pieces/white/King.scala
@@ -3,9 +3,11 @@ package game.pieces.white
 import game.{RelativePositionRange, ShortMoveRanges}
 
 object King extends game.pieces.King with WhitePiece {
-  // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] =
-    Set(ShortMoveRanges.moveForward)
+    Set(ShortMoveRanges.moveForward, ShortMoveRanges.moveRight,
+      ShortMoveRanges.moveBack, ShortMoveRanges.moveLeft,
+      ShortMoveRanges.moveNortheast, ShortMoveRanges.moveNorthwest,
+      ShortMoveRanges.moveSouthwest, ShortMoveRanges.moveSoutheast)
   // TODO: Write tests for this
   override val canJumpOver: Boolean = true
   // TODO: Write tests for this
