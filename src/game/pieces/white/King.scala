870395e4f8b5b4ac540e10726379b7b65d031df8
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-05 00:13:35 -0400

    Passing possible captures test

--- src/game/pieces/white/King.scala
+++ src/game/pieces/white/King.scala
@@ -4,9 +4,11 @@ import game.{RelativePositionRange, ShortMoveRanges}
 
 object King extends game.pieces.King with WhitePiece {
   override val captureSameAsMove: Boolean = true
-  // TODO: Write tests for this
   override def possibleCaptures: Set[RelativePositionRange] =
-    Set(ShortMoveRanges.moveBack)
+    Set(ShortMoveRanges.moveForward, ShortMoveRanges.moveRight,
+      ShortMoveRanges.moveBack, ShortMoveRanges.moveLeft,
+      ShortMoveRanges.moveNortheast, ShortMoveRanges.moveNorthwest,
+      ShortMoveRanges.moveSouthwest, ShortMoveRanges.moveSoutheast)
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = false
 
