1f4c40493c6868781bd680ee6eb9da751bcd052b
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-05 00:15:49 -0400

    Refactored capture same as move by deleting override

--- src/game/pieces/white/King.scala
+++ src/game/pieces/white/King.scala
@@ -1,9 +1,6 @@
 package game.pieces.white
 
-import game.{RelativePositionRange, ShortMoveRanges}
-
 object King extends game.pieces.King with WhitePiece {
-  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = false
 
