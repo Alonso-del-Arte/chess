80deb37036077f02c2c1b56d3bf4f47b6c782c21
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-04 23:55:36 -0400

    Refactored canJumpOver by deleting override

--- src/game/pieces/white/King.scala
+++ src/game/pieces/white/King.scala
@@ -4,8 +4,6 @@ import game.{RelativePositionRange, ShortMoveRanges}
 
 object King extends game.pieces.King with WhitePiece {
   // TODO: Write tests for this
-  override val canJumpOver: Boolean = false
-  // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = false
