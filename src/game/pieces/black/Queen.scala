f5e48f00a1bbfe935edcb7683026b0c16d97edb6
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-05 23:30:50 -0400

    Preparing to fail the next few tests

--- src/game/pieces/black/Queen.scala
+++ src/game/pieces/black/Queen.scala
@@ -1,8 +1,20 @@
 package game.pieces.black
 
-import game.{Black, Player}
+import game.pieces.{Bishop, Rook}
+import game.{Black, Neutral, Player, RelativePositionRange}
 
 object Queen extends game.pieces.Queen with BlackPiece {
-  override val affiliation: Player = Black
+  // TODO: Write tests for this
+  override val affiliation: Player = Neutral
+  // TODO: Write tests for this
+  override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override def possibleCaptures: Set[RelativePositionRange] = Rook.moves
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = true
 
 }
--- src/game/pieces/white/Queen.scala
+++ src/game/pieces/white/Queen.scala
@@ -1,8 +1,20 @@
 package game.pieces.white
 
-import game.{Player, White}
+import game.pieces.{Bishop, Rook}
+import game.{Neutral, Player, RelativePositionRange, White}
 
 object Queen extends game.pieces.Queen with WhitePiece {
-  override val affiliation: Player = White
+  // TODO: Write tests for this
+  override val affiliation: Player = Neutral
+  // TODO: Write tests for this
+  override val possibleMoves: Set[RelativePositionRange] = Rook.moves
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = true
 
 }
