cfb3e4bc90ea18f789c6931d659bab738355335c
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-05 23:14:44 -0400

    Passing has special moves test

--- src/game/pieces/white/King.scala
+++ src/game/pieces/white/King.scala
@@ -1,7 +1,6 @@
 package game.pieces.white
 
 object King extends game.pieces.King with WhitePiece {
-  // TODO: Write tests for this
-  override val hasSpecialMoves: Boolean = false
+  override val hasSpecialMoves: Boolean = true
 
 }
