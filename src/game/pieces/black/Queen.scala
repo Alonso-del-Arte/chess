e34062acd6cfdc2b2638122d9981d1d7498c620a
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-08 23:57:56 -0400

    Passing hasSpecialMoves test

--- src/game/pieces/black/Queen.scala
+++ src/game/pieces/black/Queen.scala
@@ -1,7 +1,6 @@
 package game.pieces.black
 
 object Queen extends game.pieces.Queen with BlackPiece {
-  // TODO: Write tests for this
-  override val hasSpecialMoves: Boolean = true
+  override val hasSpecialMoves: Boolean = false
 
 }
