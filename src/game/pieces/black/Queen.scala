b170a07a12221a880e36c7fb8ecdbd303b093f36
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-08 23:55:05 -0400

    Removed unused imports

--- src/game/pieces/black/Queen.scala
+++ src/game/pieces/black/Queen.scala
@@ -1,8 +1,5 @@
 package game.pieces.black
 
-import game.pieces.{Bishop, Rook}
-import game.{Black, Player, RelativePositionRange}
-
 object Queen extends game.pieces.Queen with BlackPiece {
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
