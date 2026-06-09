392d3ba0ba40fb7a10166fade73cff6ae5448474
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-08 23:58:41 -0400

    Refactored hasSpecialMoves by deleting override

--- src/game/pieces/black/Queen.scala
+++ src/game/pieces/black/Queen.scala
@@ -1,6 +1,3 @@
 package game.pieces.black
 
-object Queen extends game.pieces.Queen with BlackPiece {
-  override val hasSpecialMoves: Boolean = false
-
-}
+object Queen extends game.pieces.Queen with BlackPiece
