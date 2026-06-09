18f2376fa08540e90a401fc6a7c71c27256e150d
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-08 23:47:43 -0400

    Back to passing possibleMoves test

--- src/game/pieces/black/Queen.scala
+++ src/game/pieces/black/Queen.scala
@@ -4,8 +4,8 @@ import game.pieces.{Bishop, Rook}
 import game.{Black, Player, RelativePositionRange}
 
 object Queen extends game.pieces.Queen with BlackPiece {
-  // TODO: Write tests for this
-  override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
+  override val possibleMoves: Set[RelativePositionRange] =
+    Bishop.moves ++ Rook.moves
   // TODO: Write tests for this
   override val canJumpOver: Boolean = true
   // TODO: Write tests for this
