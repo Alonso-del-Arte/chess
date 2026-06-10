852a585c85840cdd9cc5906216d55442e2cd2034
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-09 23:59:00 -0400

    Preparing to fail affiliation test

--- src/game/pieces/black/KingSideRook.scala
+++ src/game/pieces/black/KingSideRook.scala
@@ -1,9 +1,9 @@
 package game.pieces.black
 
-import game.{Black, Player}
+import game.{Black, Neutral, Player}
 import game.pieces.Rook
 
 object KingSideRook extends Rook with BlackPiece {
-  override val affiliation: Player = Black
+  override val affiliation: Player = Neutral
 
 }
