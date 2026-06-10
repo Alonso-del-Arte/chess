b0c051c8e2cccfa267825a181197414402309a84
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-09 23:57:20 -0400

    Refactored hasSpecialMoves by deleting override

--- src/game/pieces/white/Queen.scala
+++ src/game/pieces/white/Queen.scala
@@ -1,9 +1,3 @@
 package game.pieces.white
 
-import game.pieces.{Bishop, Rook}
-import game.{Player, RelativePositionRange, White}
-
-object Queen extends game.pieces.Queen with WhitePiece {
-  override val hasSpecialMoves: Boolean = false
-
-}
+object Queen extends game.pieces.Queen with WhitePiece
