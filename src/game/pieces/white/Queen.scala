d8c9fd9d45ffcc0346028c3e2afff6f103d2af68
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-09 23:49:24 -0400

    Refactored captureSameAsMove by deleting override

--- src/game/pieces/white/Queen.scala
+++ src/game/pieces/white/Queen.scala
@@ -4,7 +4,6 @@ import game.pieces.{Bishop, Rook}
 import game.{Player, RelativePositionRange, White}
 
 object Queen extends game.pieces.Queen with WhitePiece {
-  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
   // TODO: Write tests for this
