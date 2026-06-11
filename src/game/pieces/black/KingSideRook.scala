a775a4fba3054dd4fc804c7806e421fdc4a8692a
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-10 23:35:45 -0400

    Refactored hasSpecialMoves by deleting override

--- src/game/pieces/black/KingSideRook.scala
+++ src/game/pieces/black/KingSideRook.scala
@@ -1,9 +1,5 @@
 package game.pieces.black
 
-import game.{Black, Player, RelativePositionRange}
 import game.pieces.Rook
 
-object KingSideRook extends Rook with BlackPiece {
-  override val hasSpecialMoves: Boolean = false
-
-}
+object KingSideRook extends Rook with BlackPiece
