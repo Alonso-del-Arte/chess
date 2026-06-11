bba6e76af15829f65594919442322ef0c9936c87
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-10 23:25:40 -0400

    Passing hasSpecialMoves test

--- src/game/pieces/black/KingSideRook.scala
+++ src/game/pieces/black/KingSideRook.scala
@@ -4,7 +4,6 @@ import game.{Black, Player, RelativePositionRange}
 import game.pieces.Rook
 
 object KingSideRook extends Rook with BlackPiece {
-  // TODO: Write a test for this
-  override val hasSpecialMoves: Boolean = true
+  override val hasSpecialMoves: Boolean = false
 
 }
