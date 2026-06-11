ed210e4d8ebc01be636315e355364e36d3f33426
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-10 23:57:45 -0400

    Back to passing canJumpOver test

--- src/game/pieces/black/QueenSideRook.scala
+++ src/game/pieces/black/QueenSideRook.scala
@@ -4,8 +4,7 @@ import game.RelativePositionRange
 import game.pieces.Rook
 
 object QueenSideRook extends Rook with BlackPiece {
-  // TODO: Write a test for this
-  override val canJumpOver: Boolean = true
+  override val canJumpOver: Boolean = false
   // TODO: Write a test for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write a test for this
