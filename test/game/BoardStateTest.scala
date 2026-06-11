0ce867c983ce2dacdcd2e6264806a86c52e094b3
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-11 00:24:16 -0400

    Preparations for next test

--- test/game/BoardStateTest.scala
+++ test/game/BoardStateTest.scala
@@ -1,5 +1,25 @@
 package game
 
+import game.pieces.darkgray.DarkGrayPawn
+
+import scala.util.Random
+
+object BoardStateTest {
+
+  private val DARK_GRAY_PAWNS: Array[DarkGrayPawn] =
+    Array(game.pieces.darkgray.TowersPawn, game.pieces.darkgray.HorsesPawn,
+      game.pieces.darkgray.PointyHatGuysPawn,
+      game.pieces.darkgray.EmpressesPawn, game.pieces.darkgray.EmperorsPawn)
+
+  private val NUMBER_OF_DARK_GRAY_PAWNS: Int = DARK_GRAY_PAWNS.length
+
+  def chooseDarkGrayPawn: DarkGrayPawn = {
+    val index = Random.nextInt(NUMBER_OF_DARK_GRAY_PAWNS)
+    DARK_GRAY_PAWNS(index)
+  }
+
+}
+
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
