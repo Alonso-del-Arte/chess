ec0a5fab9d53931f1f378dfd96cd38891947c70f
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-12 01:43:58 -0400

    Refactored possibleCaptures by deleting override

--- src/game/pieces/black/QueenSideRook.scala
+++ src/game/pieces/black/QueenSideRook.scala
@@ -1,10 +1,8 @@
 package game.pieces.black
 
-import game.RelativePositionRange
 import game.pieces.Rook
 
 object QueenSideRook extends Rook with BlackPiece {
-  override val possibleCaptures: Set[RelativePositionRange] = Rook.moves
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = true
 
