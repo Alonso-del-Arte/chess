2d239e3e5510ea4b80a8332d5084ef32f969987d
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-12 02:05:50 -0400

    Refactored couldCastle by deleting override

--- src/game/pieces/black/KingSideRook.scala
+++ src/game/pieces/black/KingSideRook.scala
@@ -2,7 +2,4 @@ package game.pieces.black
 
 import game.pieces.Rook
 
-object KingSideRook extends Rook with BlackPiece {
-  override val couldCastle: Boolean = true
-
-}
+object KingSideRook extends Rook with BlackPiece
