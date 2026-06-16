be02971e2c75e3fcf0c9f616e3d350872548ae6a
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-16 16:01:15 -0400

    Passing could castle test

--- src/game/pieces/white/QueenSideRook.scala
+++ src/game/pieces/white/QueenSideRook.scala
@@ -3,7 +3,6 @@ package game.pieces.white
 import game.pieces.Rook
 
 object QueenSideRook extends Rook with WhitePiece {
-  // TODO: Write a test for this
-  override val couldCastle: Boolean = false
+  override val couldCastle: Boolean = true
 
 }
