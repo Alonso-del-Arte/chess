f93f07bceef2e6c13195e8bd2dcb928fa9ffba6d
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-15 23:56:09 -0400

    Refactored possible moves by deleting override

--- src/game/pieces/white/QueenSideRook.scala
+++ src/game/pieces/white/QueenSideRook.scala
@@ -4,7 +4,6 @@ import game.{Player, RelativePositionRange, White}
 import game.pieces.Rook
 
 object QueenSideRook extends Rook with WhitePiece {
-  override val possibleMoves: Set[RelativePositionRange] = Rook.moves
   // TODO: Write a test for this
   override val canJumpOver: Boolean = true
   // TODO: Write a test for this
