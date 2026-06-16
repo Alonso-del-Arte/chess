ee80337f86d8ed2ca54bc08e26ce3dbd6438eb97
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-16 15:58:21 -0400

    Refactored hasSpecialMoves by deleting override

--- src/game/pieces/white/QueenSideRook.scala
+++ src/game/pieces/white/QueenSideRook.scala
@@ -3,7 +3,6 @@ package game.pieces.white
 import game.pieces.Rook
 
 object QueenSideRook extends Rook with WhitePiece {
-  override val hasSpecialMoves: Boolean = false
   // TODO: Write a test for this
   override val couldCastle: Boolean = false
 
