448cd870554498158865cfb835434d90c7f47fd9
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-18 23:57:20 -0400

    Back to passing canJumpOver test

--- test/game/pieces/Tower.scala
+++ test/game/pieces/Tower.scala
@@ -8,8 +8,7 @@ import game.RelativePositionRange
  * @author Alonso del Arte
  */
 abstract class Tower extends Rook {
-  // TODO: Write a test for this
-  override val canJumpOver: Boolean = true
+  override val canJumpOver: Boolean = false
   // TODO: Write a test for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write a test for this
