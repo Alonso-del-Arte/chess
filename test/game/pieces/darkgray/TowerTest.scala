b0f3907ac8664a33fa2ea457b2f647b51572f355
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-18 23:42:47 -0400

    Failing affiliation test

--- test/game/pieces/darkgray/TowerTest.scala
+++ test/game/pieces/darkgray/TowerTest.scala
@@ -1,8 +1,15 @@
 package game.pieces.darkgray
 
+import game.DarkGray
+
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class TowerTest {
+
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    assertEquals(DarkGray, Tower.affiliation)
+  }
 
 }
