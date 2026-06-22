e0475799926fa63e29e4e0b12fd2070d1de1ea7b
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-22 15:20:07 -0400

    Passing possible captures test

--- test/game/pieces/Tower.scala
+++ test/game/pieces/Tower.scala
@@ -8,8 +8,7 @@ import game.RelativePositionRange
  * @author Alonso del Arte
  */
 abstract class Tower extends Rook {
-  // TODO: Write a test for this
-  override val possibleCaptures: Set[RelativePositionRange] = Set()
+  override val possibleCaptures: Set[RelativePositionRange] = Rook.moves
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = true
   // TODO: Write a test for this
