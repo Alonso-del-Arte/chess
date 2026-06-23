fa5d41864e7464541e8fcba596f1cbeaf33abfb0
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-22 23:24:42 -0400

    Removed unused import

--- test/game/pieces/Tower.scala
+++ test/game/pieces/Tower.scala
@@ -1,7 +1,5 @@
 package game.pieces
 
-import game.RelativePositionRange
-
 /**
  * The tower is a chess piece to be used strictly for testing purposes only. The
  * tower has the same moves as the rook.
