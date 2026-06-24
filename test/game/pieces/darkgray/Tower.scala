646d96b2f857a28556dddbe7f6b201214a955476
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-24 00:52:43 -0400

    Passing hasSpecialMoves test

--- test/game/pieces/darkgray/Tower.scala
+++ test/game/pieces/darkgray/Tower.scala
@@ -4,8 +4,7 @@ import game.pieces.Rook
 import game.{Player, RelativePositionRange}
 
 object Tower extends game.pieces.Tower with DarkGrayPiece {
-  // TODO: Write a test for this
-  override val hasSpecialMoves: Boolean = true
+  override val hasSpecialMoves: Boolean = false
   // TODO: Write a test for this
   override val couldCastle: Boolean = false
 
