b6ae51727a2da8c20e34557b2fefa2db682159ea
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-23 23:44:18 -0400

    Refactored possible moves by deleting override

--- test/game/pieces/darkgray/Tower.scala
+++ test/game/pieces/darkgray/Tower.scala
@@ -4,7 +4,6 @@ import game.pieces.Rook
 import game.{Player, RelativePositionRange}
 
 object Tower extends game.pieces.Tower with DarkGrayPiece {
-  override val possibleMoves: Set[RelativePositionRange] = Rook.moves
   // TODO: Write a test for this
   override val canJumpOver: Boolean = true
   // TODO: Write a test for this
