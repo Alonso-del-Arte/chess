8180d163452822b3898f03a59ae39cbbc3d41c67
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-24 00:47:21 -0400

    Refactored capture same as move by deleting override

--- test/game/pieces/darkgray/Tower.scala
+++ test/game/pieces/darkgray/Tower.scala
@@ -4,7 +4,6 @@ import game.pieces.Rook
 import game.{Player, RelativePositionRange}
 
 object Tower extends game.pieces.Tower with DarkGrayPiece {
-  override val captureSameAsMove: Boolean = true
   // TODO: Write a test for this
   override val possibleCaptures: Set[RelativePositionRange] = Set()
   // TODO: Write a test for this
