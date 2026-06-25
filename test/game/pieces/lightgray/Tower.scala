f5f9fc32cd0a4a7dc90183f44e40402692d30d6d
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-25 01:19:02 -0400

    Refactored could castle by deleting override

--- test/game/pieces/lightgray/Tower.scala
+++ test/game/pieces/lightgray/Tower.scala
@@ -1,6 +1,3 @@
 package game.pieces.lightgray
 
-object Tower extends game.pieces.Tower with LightGrayPiece {
-  override val couldCastle: Boolean = true
-
-}
+object Tower extends game.pieces.Tower with LightGrayPiece
