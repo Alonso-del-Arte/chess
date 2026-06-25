8f62f99e1d4ea710523a76d779b7edf9653d37ad
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-25 01:16:43 -0400

    Refactored has special moves by deleting override

--- test/game/pieces/lightgray/Tower.scala
+++ test/game/pieces/lightgray/Tower.scala
@@ -1,7 +1,6 @@
 package game.pieces.lightgray
 
 object Tower extends game.pieces.Tower with LightGrayPiece {
-  override val hasSpecialMoves: Boolean = false
   // TODO: Write a test for this
   override val couldCastle: Boolean = false
 
