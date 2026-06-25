be08ad6c5b2e9ce1b7bdb4c3b4a73a1e161a12ad
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-25 01:13:51 -0400

    Refactored possible captures by deleting override

--- test/game/pieces/lightgray/Tower.scala
+++ test/game/pieces/lightgray/Tower.scala
@@ -1,10 +1,6 @@
 package game.pieces.lightgray
 
-import game.RelativePositionRange
-import game.pieces.Rook
-
 object Tower extends game.pieces.Tower with LightGrayPiece {
-  override val possibleCaptures: Set[RelativePositionRange] = Rook.moves
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = true
   // TODO: Write a test for this
