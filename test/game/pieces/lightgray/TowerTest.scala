82ffac775fa73e3ab1cca1bae2d57fd193d855db
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-24 20:30:10 -0400

    Failing canJumpOver test

--- test/game/pieces/lightgray/TowerTest.scala
+++ test/game/pieces/lightgray/TowerTest.scala
@@ -20,4 +20,10 @@ class TowerTest {
     assertEquals(expected, actual)
   }
 
+  @Test def testCanJumpOver(): Unit = {
+    println("canJumpOver")
+    val msg = "Tower shouldn't be able to jump over"
+    assert(!Tower.canJumpOver, msg)
+  }
+
 }
