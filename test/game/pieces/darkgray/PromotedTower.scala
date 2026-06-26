cbd4983ed5ae702397fc7088bf3e924ff31e0711
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-26 17:06:41 -0400

    Refactored possible captures by deleting override

--- test/game/pieces/darkgray/PromotedTower.scala
+++ test/game/pieces/darkgray/PromotedTower.scala
@@ -5,7 +5,6 @@ import game.pieces.{NeutralPawn, Pawn, Promoted, Rook, Tower}
 
 class PromotedTower(val pawn: Pawn) extends Tower with DarkGrayPiece
   with Promoted {
-  override val possibleCaptures: Set[RelativePositionRange] = Rook.moves
   // TODO: Write a test for this
   override val couldCastle: Boolean = true
   // TODO: Write a test for this
