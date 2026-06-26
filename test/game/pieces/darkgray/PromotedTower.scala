6f3fb0867b245a81bd58b4d183efcabed0e5746a
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-26 16:56:06 -0400

    Preparing to fail the next couple of tests

--- test/game/pieces/darkgray/PromotedTower.scala
+++ test/game/pieces/darkgray/PromotedTower.scala
@@ -6,9 +6,9 @@ import game.pieces.{NeutralPawn, Pawn, Promoted, Tower}
 class PromotedTower(val pawn: Pawn) extends Tower with DarkGrayPiece
   with Promoted {
   // TODO: Write a test for this
-  override val canJumpOver: Boolean = false
+  override val canJumpOver: Boolean = true
   // TODO: Write a test for this
-  override val captureSameAsMove: Boolean = true
+  override val captureSameAsMove: Boolean = false
   // TODO: Write a test for this
   override val possibleCaptures: Set[RelativePositionRange] = Set()
   // TODO: Write a test for this
