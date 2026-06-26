d8451b2b38e6636451eb1aaad07d0c5271dd480c
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-26 16:45:59 -0400

    Preparing to fail the next few tests

--- test/game/pieces/lightgray/PromotedTower.scala
+++ test/game/pieces/lightgray/PromotedTower.scala
@@ -1,11 +1,24 @@
 package game.pieces.lightgray
 
-import game.pieces.{Pawn, Promoted, Tower}
+import game.{Neutral, Player, RelativePositionRange}
+import game.pieces.{NeutralPawn, Pawn, Promoted, Tower}
 
 class PromotedTower(val pawn: Pawn) extends Tower with LightGrayPiece
   with Promoted {
   // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
+  override val possibleMoves: Set[RelativePositionRange] = Set()
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = false
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
+  // TODO: Write a test for this
+  override val possibleCaptures: Set[RelativePositionRange] = Set()
+  // TODO: Write a test for this
   override val couldCastle: Boolean = true
-  override def prior: Pawn = pawn
+  // TODO: Write a test for this
+  override def prior: Pawn = NeutralPawn
 
 }
+
