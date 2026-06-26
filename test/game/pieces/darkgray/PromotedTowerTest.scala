a6e2c672ffe4d21b4181b12c826d02c45f4201d2
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-26 16:49:20 -0400

    Failing affiliation test

--- test/game/pieces/darkgray/PromotedTowerTest.scala
+++ test/game/pieces/darkgray/PromotedTowerTest.scala
@@ -1,8 +1,20 @@
 package game.pieces.darkgray
 
+import game.DarkGray
+import game.pieces.PieceChooser
+
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class PromotedTowerTest {
+
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    val expected = DarkGray
+    val pawn = PieceChooser.choosePawn(expected)
+    val tower = new PromotedTower(pawn)
+    val actual = tower.affiliation
+    assertEquals(expected, actual)
+  }
 
 }
