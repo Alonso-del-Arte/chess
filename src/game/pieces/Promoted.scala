447bcbe51b4f5abd3a4554655ef9aa36966c67c6
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-26 23:53:36 -0400

    Documentation

--- src/game/pieces/Promoted.scala
+++ src/game/pieces/Promoted.scala
@@ -1,7 +1,15 @@
 package game.pieces
 
+/**
+ * Characteristic of a piece promoted from a pawn.
+ */
 trait Promoted {
 
+  /**
+   * Retrieves the pawn this piece was promoted from.
+   * @return The pawn this piece was promoted from. For example, White queenside
+   *         rook's pawn.
+   */
   def prior: Pawn
 
 }
