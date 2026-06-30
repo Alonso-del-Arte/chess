1d2769694873acb9537d289488dd7a067bebb0be
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-30 01:09:30 -0400

    Preparing to fail the next few tests

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -1,9 +1,23 @@
 package game.pieces.darkgray
 
-import game.pieces.{Pawn, PointyHatGuy, Promoted}
+import game.{Neutral, Player, RelativePositionRange}
+import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
+  // TODO: Write tests for this
+  override val affiliation: Player = Neutral
+  // TODO: Write tests for this
+  override val possibleMoves: Set[RelativePositionRange] = Set()
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = true
+  // TODO: Write tests for this
+  override def possibleCaptures: Set[RelativePositionRange] =
+    Set(Bishop.moveNorthwest)
 
   override def prior: Pawn = pawn
 
--- test/game/pieces/lightgray/PointyHatGuy.scala
+++ test/game/pieces/lightgray/PointyHatGuy.scala
@@ -1,5 +1,21 @@
 package game.pieces.lightgray
 
+import game.{Neutral, Player, RelativePositionRange}
+import game.pieces.Bishop
+
 object PointyHatGuy extends game.pieces.PointyHatGuy with LightGrayPiece {
+  // TODO: Write tests for this
+  override val affiliation: Player = Neutral
+  // TODO: Write tests for this
+  override val possibleMoves: Set[RelativePositionRange] = Set()
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = true
+  // TODO: Write tests for this
+  override def possibleCaptures: Set[RelativePositionRange] =
+    Set(Bishop.moveNorthwest)
 
 }
