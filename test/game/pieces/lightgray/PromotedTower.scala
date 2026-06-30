caa838c110da3d71b1223dfb635b0bf85d75ef5b
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-29 23:58:54 -0400

    Refactored capture same as move by deleting override

--- test/game/pieces/lightgray/PromotedTower.scala
+++ test/game/pieces/lightgray/PromotedTower.scala
@@ -5,7 +5,6 @@ import game.pieces.{NeutralPawn, Pawn, Promoted, Rook, Tower}
 
 class PromotedTower(val pawn: Pawn) extends Tower with LightGrayPiece
   with Promoted {
-  override val captureSameAsMove: Boolean = true
   // TODO: Write a test for this
   override val possibleCaptures: Set[RelativePositionRange] = Set()
   // TODO: Write a test for this
