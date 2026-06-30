28714083dc4a83a21bf4933492aefca852898144
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-30 00:49:27 -0400

    Passing prior test

--- test/game/pieces/lightgray/PromotedTower.scala
+++ test/game/pieces/lightgray/PromotedTower.scala
@@ -6,7 +6,7 @@ import game.pieces.{NeutralPawn, Pawn, Promoted, Rook, Tower}
 class PromotedTower(val pawn: Pawn) extends Tower with LightGrayPiece
   with Promoted {
   override val couldCastle: Boolean = false
-  // TODO: Write a test for this
-  override def prior: Pawn = NeutralPawn
+
+  override def prior: Pawn = pawn
 
 }
