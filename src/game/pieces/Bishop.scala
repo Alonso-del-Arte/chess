247765c0544ed468b011219a10079e7f67eef482
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-30 01:21:09 -0400

    Back to passing possible captures test

--- src/game/pieces/Bishop.scala
+++ src/game/pieces/Bishop.scala
@@ -27,8 +27,6 @@ object Bishop {
 
 abstract class Bishop extends Piece {
   override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
-  // TODO: Write tests for this
-  override def possibleCaptures: Set[RelativePositionRange] =
-    Set(Bishop.moveNorthwest)
+  override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
 
 }
