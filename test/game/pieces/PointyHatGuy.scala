ee86d77abd3e3765b532b89eb8ca8b357c9398aa
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-30 01:06:51 -0400

    Made a subclass of Bishop and preparing to fail the next few tests

--- test/game/pieces/PointyHatGuy.scala
+++ test/game/pieces/PointyHatGuy.scala
@@ -7,7 +7,17 @@ import game.RelativePositionRange
  * only. The pointy hat guy has the same moves as the bishop.
  * @author Alonso del Arte
  */
-abstract class PointyHatGuy extends Piece {
+abstract class PointyHatGuy extends Bishop {
+  // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = true
+  // TODO: Write tests for this
+  override def possibleCaptures: Set[RelativePositionRange] =
+    Set(Bishop.moveNorthwest)
 
 }
