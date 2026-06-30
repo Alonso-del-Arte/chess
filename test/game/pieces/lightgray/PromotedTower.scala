c03b8dc55030c7aec5a56ce2246b0670fa67aef0
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-29 23:59:28 -0400

    Passing possible captures test

--- test/game/pieces/lightgray/PromotedTower.scala
+++ test/game/pieces/lightgray/PromotedTower.scala
@@ -5,8 +5,7 @@ import game.pieces.{NeutralPawn, Pawn, Promoted, Rook, Tower}
 
 class PromotedTower(val pawn: Pawn) extends Tower with LightGrayPiece
   with Promoted {
-  // TODO: Write a test for this
-  override val possibleCaptures: Set[RelativePositionRange] = Set()
+  override val possibleCaptures: Set[RelativePositionRange] = Rook.moves
   // TODO: Write a test for this
   override val couldCastle: Boolean = true
   // TODO: Write a test for this
