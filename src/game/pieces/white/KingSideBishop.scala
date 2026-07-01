70044b654ca1ef9f7074e5c526967411a05a6d04
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-01 02:43:20 -0400

    Back to passing affiliation test

--- src/game/pieces/white/KingSideBishop.scala
+++ src/game/pieces/white/KingSideBishop.scala
@@ -4,8 +4,7 @@ import game.{Neutral, Player, RelativePositionRange, White}
 import game.pieces.Bishop
 
 object KingSideBishop extends Bishop with WhitePiece {
-  // TODO: Write tests for this
-  override val affiliation: Player = Neutral
+  override val affiliation: Player = White
   // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
   // TODO: Write tests for this
