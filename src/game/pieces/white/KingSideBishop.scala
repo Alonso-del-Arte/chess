fea7815e85d92d0f86b00daa3cd0f228f42392da
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-01 02:44:13 -0400

    Refactored affiliation by deleting override

--- src/game/pieces/white/KingSideBishop.scala
+++ src/game/pieces/white/KingSideBishop.scala
@@ -1,10 +1,9 @@
 package game.pieces.white
 
-import game.{Neutral, Player, RelativePositionRange, White}
+import game.RelativePositionRange
 import game.pieces.Bishop
 
 object KingSideBishop extends Bishop with WhitePiece {
-  override val affiliation: Player = White
   // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
   // TODO: Write tests for this
