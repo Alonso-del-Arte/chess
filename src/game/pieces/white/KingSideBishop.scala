05cfd2b4cc50572d99f51d84adb63b234c5de041
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-01 22:49:00 -0400

    Refactored possible captures by deleting override

--- src/game/pieces/white/KingSideBishop.scala
+++ src/game/pieces/white/KingSideBishop.scala
@@ -6,7 +6,5 @@ import game.pieces.Bishop
 object KingSideBishop extends Bishop with WhitePiece {
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
-  // TODO: Write tests for this
-  override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
 
 }
