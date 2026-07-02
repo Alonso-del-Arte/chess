2d78698c60789782542d4b6c32b26a02f4547b1a
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-02 18:35:19 -0400

    Refactored

--- src/game/pieces/white/KingSideBishop.scala
+++ src/game/pieces/white/KingSideBishop.scala
@@ -1,9 +1,5 @@
 package game.pieces.white
 
-import game.RelativePositionRange
 import game.pieces.Bishop
 
-object KingSideBishop extends Bishop with WhitePiece {
-  override val hasSpecialMoves: Boolean = false
-
-}
+object KingSideBishop extends Bishop with WhitePiece
