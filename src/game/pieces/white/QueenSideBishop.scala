1611c1caf4bdd3d78dd10bf775dc351648d825f0
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-02 23:06:00 -0400

    Back to passing possible captures test

--- src/game/pieces/white/QueenSideBishop.scala
+++ src/game/pieces/white/QueenSideBishop.scala
@@ -6,8 +6,6 @@ import game.pieces.Bishop
 object QueenSideBishop extends Bishop with WhitePiece {
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
-  // TODO: Write tests for this
-  override def possibleCaptures: Set[RelativePositionRange] =
-    Set(Bishop.moveNorthwest)
+  override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
 
 }
