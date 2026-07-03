6597b5c5beda7117ba9ae8a25fae30fe90223290
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-02 23:47:11 -0400

    Back to passing affiliation test

--- src/game/pieces/black/QueenSideBishop.scala
+++ src/game/pieces/black/QueenSideBishop.scala
@@ -1,11 +1,10 @@
 package game.pieces.black
 
-import game.{Black, Neutral, Player, RelativePositionRange}
+import game.{Black, Player, RelativePositionRange}
 import game.pieces.Bishop
 
 object QueenSideBishop extends Bishop with BlackPiece {
-  // TODO: Write tests for this
-  override val affiliation: Player = Neutral
+  override val affiliation: Player = Black
   // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
   // TODO: Write tests for this
