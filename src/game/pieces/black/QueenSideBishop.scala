1c6cf68974ea2a9a02490bec2603730852ea3d7f
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-06 15:15:00 -0400

    Refactored hasSpecialMoves by deleting override

--- src/game/pieces/black/QueenSideBishop.scala
+++ src/game/pieces/black/QueenSideBishop.scala
@@ -1,9 +1,5 @@
 package game.pieces.black
 
-import game.RelativePositionRange
 import game.pieces.Bishop
 
-object QueenSideBishop extends Bishop with BlackPiece {
-  override val hasSpecialMoves: Boolean = false
-
-}
+object QueenSideBishop extends Bishop with BlackPiece
