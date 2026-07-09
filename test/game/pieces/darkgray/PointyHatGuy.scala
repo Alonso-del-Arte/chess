f18cd69385d7573212cc91551d71b63cea87149f
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-09 00:02:52 -0400

    Passing possible captures test

--- test/game/pieces/darkgray/PointyHatGuy.scala
+++ test/game/pieces/darkgray/PointyHatGuy.scala
@@ -6,8 +6,6 @@ import game.pieces.Bishop
 object PointyHatGuy extends game.pieces.PointyHatGuy with DarkGrayPiece {
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
-  // TODO: Write tests for this
-  override def possibleCaptures: Set[RelativePositionRange] =
-    Set(Bishop.moveNorthwest)
+  override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
 
 }
