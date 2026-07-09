5a8cb51fe2263fe96be94ede83acaf7320fd98c4
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-08 23:45:02 -0400

    Removed unused imports

--- test/game/pieces/PointyHatGuy.scala
+++ test/game/pieces/PointyHatGuy.scala
@@ -1,7 +1,5 @@
 package game.pieces
 
-import game.RelativePositionRange
-
 /**
  * The pointy hat guy is a chess piece to be used strictly for testing purposes
  * only. The pointy hat guy has the same moves as the bishop.
