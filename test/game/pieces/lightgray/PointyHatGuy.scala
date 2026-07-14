805c1c0638e306fcc5bf12f598a492f24243d75a
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-13 23:59:08 -0400

    Refactored hasSpecialMoves by deleting override

--- test/game/pieces/lightgray/PointyHatGuy.scala
+++ test/game/pieces/lightgray/PointyHatGuy.scala
@@ -1,6 +1,3 @@
 package game.pieces.lightgray
 
-object PointyHatGuy extends game.pieces.PointyHatGuy with LightGrayPiece {
-  override val hasSpecialMoves: Boolean = false
-
-}
+object PointyHatGuy extends game.pieces.PointyHatGuy with LightGrayPiece
