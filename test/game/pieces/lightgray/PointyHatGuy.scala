8638b9b78b6a53f7931bebc532f21f1f9ff34739
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-13 23:56:33 -0400

    Refactored possible captures by deleting override

--- test/game/pieces/lightgray/PointyHatGuy.scala
+++ test/game/pieces/lightgray/PointyHatGuy.scala
@@ -1,11 +1,7 @@
 package game.pieces.lightgray
 
-import game.RelativePositionRange
-import game.pieces.Bishop
-
 object PointyHatGuy extends game.pieces.PointyHatGuy with LightGrayPiece {
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
-  override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
 
 }
