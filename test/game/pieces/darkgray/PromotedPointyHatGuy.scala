e6fe9e3228d8763ddc7a52ac2467234a1e10bb37
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-14 00:18:31 -0400

    Passing canJumpOver test

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -5,8 +5,7 @@ import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
-  // TODO: Write tests for this
-  override val canJumpOver: Boolean = true
+  override val canJumpOver: Boolean = false
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
