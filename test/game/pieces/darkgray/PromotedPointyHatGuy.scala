62ec6f45272e7c10194a6c6cfaebe5cc499fa8d4
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-14 00:14:06 -0400

    Refactored possible moves by deleting override

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -1,11 +1,10 @@
 package game.pieces.darkgray
 
-import game.{DarkGray, Player, RelativePositionRange}
+import game.RelativePositionRange
 import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
-  override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
   // TODO: Write tests for this
   override val canJumpOver: Boolean = true
   // TODO: Write tests for this
