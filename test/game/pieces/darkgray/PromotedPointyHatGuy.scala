90858d53949eaaf3c09a94155c77629a732105d5
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-14 22:15:14 -0400

    Refactored capture same as move by deleting override

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -5,7 +5,6 @@ import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
-  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
   // TODO: Write tests for this
