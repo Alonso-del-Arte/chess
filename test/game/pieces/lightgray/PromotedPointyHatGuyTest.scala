21b5b98f36157778b0c5468dc47d590603feabef
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-15 00:20:05 -0400

    New test class

---
+++ test/game/pieces/lightgray/PromotedPointyHatGuyTest.scala
@@ -0,0 +1,9 @@
+package game.pieces.lightgray
+
+import game.LightGray
+import game.pieces.{Bishop, PieceChooser}
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class PromotedPointyHatGuyTest
