27ab8520209abe4b0835be19e584b78a0b26415f
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-14 22:21:15 -0400

    Refactored possible captures by deleting override

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -1,13 +1,11 @@
 package game.pieces.darkgray
 
-import game.RelativePositionRange
-import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
+import game.pieces.{Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
-  override def possibleCaptures: Set[RelativePositionRange] = Bishop.moves
 
   override def prior: Pawn = pawn
 
