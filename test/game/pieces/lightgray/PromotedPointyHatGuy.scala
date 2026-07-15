a06f8ad33c206818a5ace0adb3fca27029fe92ae
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-15 00:24:57 -0400

    Refactored affiliation by deleting override

--- test/game/pieces/lightgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/lightgray/PromotedPointyHatGuy.scala
@@ -5,7 +5,6 @@ import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with LightGrayPiece with Promoted {
-  override val affiliation: Player = LightGray
   // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
   // TODO: Write tests for this
