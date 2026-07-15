e88f69d890249704a4800fa46eb15cacea2cd6ca
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-14 22:13:49 -0400

    Passing captures same as moves test

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -5,8 +5,7 @@ import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
-  // TODO: Write tests for this
-  override val captureSameAsMove: Boolean = false
+  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
   // TODO: Write tests for this
