a14945713f209c9ce5ab10ada10ff6ddc5b89021
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-14 23:49:40 -0400

    Refactored hasSpecialMoves by deleting override

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -4,7 +4,6 @@ import game.pieces.{Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
-  override val hasSpecialMoves: Boolean = false
 
   override def prior: Pawn = pawn
 
