6c63fdf462c01bb45a91e74017f340425058f3f9
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-15 00:23:20 -0400

    Failing affiliation test

--- test/game/pieces/lightgray/PromotedPointyHatGuyTest.scala
+++ test/game/pieces/lightgray/PromotedPointyHatGuyTest.scala
@@ -6,4 +6,15 @@ import game.pieces.{Bishop, PieceChooser}
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
-class PromotedPointyHatGuyTest
+class PromotedPointyHatGuyTest {
+
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    val expected = LightGray
+    val pawn = PieceChooser.choosePawn(expected)
+    val pointyHatGuy = new PromotedPointyHatGuy(pawn)
+    val actual = pointyHatGuy.affiliation
+    assertEquals(expected, actual)
+  }
+
+}
