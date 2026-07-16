8be2f953d6edcc9b113d3bc4bbe4debe6f05211c
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 16:32:04 -0400

    Passing capture same as move test

--- test/game/pieces/lightgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/lightgray/PromotedPointyHatGuy.scala
@@ -5,8 +5,7 @@ import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with LightGrayPiece with Promoted {
-  // TODO: Write tests for this
-  override val captureSameAsMove: Boolean = false
+  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override def possibleCaptures: Set[RelativePositionRange] =
     Set(Bishop.moveNorthwest, Bishop.moveSoutheast)
