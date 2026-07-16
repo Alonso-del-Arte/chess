251f67c48ad0961a929e24d8fea12d6d608a787d
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 17:15:32 -0400

    Preparing to fail prior test

--- test/game/pieces/darkgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/darkgray/PromotedPointyHatGuy.scala
@@ -1,10 +1,10 @@
 package game.pieces.darkgray
 
-import game.pieces.{Pawn, PointyHatGuy, Promoted}
+import game.pieces.{NeutralPawn, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with DarkGrayPiece with Promoted {
 
-  override def prior: Pawn = pawn
+  override def prior: Pawn = NeutralPawn
 
 }
