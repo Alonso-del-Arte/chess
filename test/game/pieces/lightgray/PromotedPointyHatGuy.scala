a004b8aa77d1b1da5955e59f6ac60a14f0147fef
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 16:32:36 -0400

    Refactored capture same as move by deleting override

--- test/game/pieces/lightgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/lightgray/PromotedPointyHatGuy.scala
@@ -5,7 +5,6 @@ import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with LightGrayPiece with Promoted {
-  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override def possibleCaptures: Set[RelativePositionRange] =
     Set(Bishop.moveNorthwest, Bishop.moveSoutheast)
