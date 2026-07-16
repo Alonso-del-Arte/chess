964082b277008839ff16a737c0b5a305fa45fe79
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 18:02:51 -0400

    Failing affiliation test

--- test/game/pieces/PromotedEmperorTest.scala
+++ test/game/pieces/PromotedEmperorTest.scala
@@ -1,6 +1,22 @@
 package game.pieces
 
+import game.{DarkGray, LightGray}
+
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
-class PromotedEmperorTest
+class PromotedEmperorTest {
+
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    val promotedEmperorDark =
+      new PromotedEmperor(PieceChooser.choosePawn(DarkGray))
+    val promotedEmperorLight = 
+      new PromotedEmperor(PieceChooser.choosePawn(LightGray))
+    assertEquals(DarkGray, promotedEmperorDark.affiliation,
+      s"Affiliation of $promotedEmperorDark should be $DarkGray")
+    assertEquals(LightGray, promotedEmperorLight.affiliation,
+      s"Affiliation of $promotedEmperorLight should be $LightGray")
+  }
+
+}
