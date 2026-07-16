b70978f14bbfc6e3fc8db0f799686b6a1a6647e7
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 17:12:31 -0400

    Removed unused imports

--- test/game/pieces/lightgray/PromotedTower.scala
+++ test/game/pieces/lightgray/PromotedTower.scala
@@ -1,7 +1,6 @@
 package game.pieces.lightgray
 
-import game.RelativePositionRange
-import game.pieces.{NeutralPawn, Pawn, Promoted, Rook, Tower}
+import game.pieces.{Pawn, Promoted, Tower}
 
 class PromotedTower(val pawn: Pawn) extends Tower with LightGrayPiece
   with Promoted {
