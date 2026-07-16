cd18b29d32a1aa1680192c4684f4f574e1a5d859
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 16:30:52 -0400

    Refactored canJumpOver by deleting override

--- test/game/pieces/lightgray/PromotedPointyHatGuy.scala
+++ test/game/pieces/lightgray/PromotedPointyHatGuy.scala
@@ -5,7 +5,6 @@ import game.pieces.{Bishop, Pawn, PointyHatGuy, Promoted}
 
 class PromotedPointyHatGuy(val pawn: Pawn) extends PointyHatGuy
   with LightGrayPiece with Promoted {
-  override val canJumpOver: Boolean = false
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
