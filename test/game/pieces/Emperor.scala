27c8ef19c3100a5c762a526dab25481aae1d33a7
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 23:52:45 -0400

    Preparation for testing

--- test/game/pieces/Emperor.scala
+++ test/game/pieces/Emperor.scala
@@ -12,6 +12,15 @@ import game.RelativePositionRange
  * @author Alonso del Arte
  */
 abstract class Emperor extends Piece {
+  // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] = Set()
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override val possibleCaptures: Set[RelativePositionRange] = Set()
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = false
 
 }
