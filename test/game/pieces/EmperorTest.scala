6f2f8c6b3028e7ba91c3918131195d96a40bed88
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 23:57:03 -0400

    Failing possible moves test

--- test/game/pieces/EmperorTest.scala
+++ test/game/pieces/EmperorTest.scala
@@ -1,11 +1,23 @@
 package game.pieces
 
-import game.{Black, Neutral, Player, RelativePositionRange, White}
+import game.{Black, Neutral, Player, RelativePositionRange, ShortMoveRanges,
+  White}
 
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class EmperorTest {
+
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val emperor = new EmperorImpl
+    val expected = Set(ShortMoveRanges.moveForward, ShortMoveRanges.moveRight,
+      ShortMoveRanges.moveBack, ShortMoveRanges.moveLeft,
+      ShortMoveRanges.moveNortheast, ShortMoveRanges.moveNorthwest,
+      ShortMoveRanges.moveSouthwest, ShortMoveRanges.moveSoutheast)
+    val actual = emperor.possibleMoves
+    assertEquals(expected, actual)
+  }
 
   @Test def testCanJumpOver(): Unit = {
     println("canJumpOver")
