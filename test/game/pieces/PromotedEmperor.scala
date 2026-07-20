e3ef4db0182eb27b0fda324a4c88913395f3c73c
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-20 15:08:03 -0400

    Reminder to extend Emperor rather than King directly

--- test/game/pieces/PromotedEmperor.scala
+++ test/game/pieces/PromotedEmperor.scala
@@ -2,6 +2,7 @@ package game.pieces
 
 import game.Player
 
+// TODO: Change to extend Emperor rather than King directly
 class PromotedEmperor(val pawn: Pawn) extends King with Promoted {
   override val affiliation: Player = this.pawn.affiliation
   // TODO: Write tests for this
