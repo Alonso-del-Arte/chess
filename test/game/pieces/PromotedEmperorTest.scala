98d983f86c11e1d34e3d7a6e294c4ad42af679ab
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-20 14:25:41 -0400

    Failing possible moves test

--- test/game/pieces/PromotedEmperorTest.scala
+++ test/game/pieces/PromotedEmperorTest.scala
@@ -1,7 +1,6 @@
 package game.pieces
 
-import game.{DarkGray, LightGray}
-
+import game.{DarkGray, LightGray, RelativePositionRange, ShortMoveRanges}
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
@@ -11,12 +10,24 @@ class PromotedEmperorTest {
     println("affiliation")
     val promotedEmperorDark =
       new PromotedEmperor(PieceChooser.choosePawn(DarkGray))
-    val promotedEmperorLight = 
+    val promotedEmperorLight =
       new PromotedEmperor(PieceChooser.choosePawn(LightGray))
     assertEquals(DarkGray, promotedEmperorDark.affiliation,
       s"Affiliation of $promotedEmperorDark should be $DarkGray")
     assertEquals(LightGray, promotedEmperorLight.affiliation,
       s"Affiliation of $promotedEmperorLight should be $LightGray")
+  }
+
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val pawn = PieceChooser.choosePawn
+    val emperor = new PromotedEmperor(pawn)
+    val expected = Set(ShortMoveRanges.moveForward, ShortMoveRanges.moveRight,
+        ShortMoveRanges.moveBack, ShortMoveRanges.moveLeft,
+        ShortMoveRanges.moveNortheast, ShortMoveRanges.moveNorthwest,
+        ShortMoveRanges.moveSouthwest, ShortMoveRanges.moveSoutheast)
+    val actual = emperor.possibleMoves
+    assertEquals(expected, actual)
   }
 
 }
