319464d1e938d31755c95a90bd9f2b60c4e4c6e6
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-20 23:59:12 -0400

    Refactored capture same as move by deleting override

--- test/game/pieces/Emperor.scala
+++ test/game/pieces/Emperor.scala
@@ -12,7 +12,6 @@ import game.{RelativePositionRange, ShortMoveRanges}
  * @author Alonso del Arte
  */
 abstract class Emperor extends King {
-  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override val possibleCaptures: Set[RelativePositionRange] = Set()
   // TODO: Write tests for this
