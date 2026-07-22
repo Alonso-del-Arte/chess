4e0b4b72585460e3778e9830f9c284b2990dc5d8
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-22 17:03:29 -0400

    Failing affiliation test

--- test/game/pieces/PromotedEmpressTest.scala
+++ test/game/pieces/PromotedEmpressTest.scala
@@ -3,4 +3,16 @@ package game.pieces
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
-class PromotedEmpressTest
+class PromotedEmpressTest {
+
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    val pawn = PieceChooser.choosePawn
+    val empress = new PromotedEmpress(pawn)
+    val expected = pawn.affiliation
+    val actual = empress.affiliation
+    val message = s"Empress promoted from $pawn should be of same side"
+    assertEquals(expected, actual, message)
+  }
+
+}
