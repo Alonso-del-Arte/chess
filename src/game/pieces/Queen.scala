21cd1cb9296f0430f8d44c97e985d7d0d604454c
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-22 16:52:06 -0400

    Refactored capture same as move by deleting override

--- src/game/pieces/Queen.scala
+++ src/game/pieces/Queen.scala
@@ -5,6 +5,5 @@ import game.RelativePositionRange
 abstract class Queen extends Piece {
   override val possibleMoves: Set[RelativePositionRange] =
     Bishop.moves ++ Rook.moves
-  override val captureSameAsMove: Boolean = true
 
 }
