cf186b9a4c99395f6eb7e5e3b5e9ecb1ee302643
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-22 16:51:25 -0400

    Refactored canJumpOver by deleting override

--- src/game/pieces/Queen.scala
+++ src/game/pieces/Queen.scala
@@ -5,7 +5,6 @@ import game.RelativePositionRange
 abstract class Queen extends Piece {
   override val possibleMoves: Set[RelativePositionRange] =
     Bishop.moves ++ Rook.moves
-  override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = true
 
 }
