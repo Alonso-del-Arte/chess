52f250f5923e7a7e7b6f93d448e0343fe4e45e97
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-22 23:57:30 -0400

    Back to passing canJumpOver

--- test/game/pieces/Empress.scala
+++ test/game/pieces/Empress.scala
@@ -13,8 +13,7 @@ import game.RelativePositionRange
  * @author Alonso del Arte
  */
 abstract class Empress extends Queen {
-  // TODO: Write tests for this
-  override val canJumpOver: Boolean = true
+  override val canJumpOver: Boolean = false
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
