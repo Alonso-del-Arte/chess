cfccc36a137a1e930961886bbaea412549caca54
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-24 23:38:51 -0400

    Passing move to 7 o'clock test

--- src/game/pieces/Knight.scala
+++ src/game/pieces/Knight.scala
@@ -20,10 +20,9 @@ object Knight {
     new RelativePositionRange(new RelativePosition(1, -2),
       new RelativePosition(1, -2))
 
-  // TODO: Write a test for this
   val moveSevenOClock: RelativePositionRange =
-    new RelativePositionRange(new RelativePosition(3, 1),
-      new RelativePosition(5, 5))
+    new RelativePositionRange(new RelativePosition(-1, -2),
+      new RelativePosition(-1, -2))
 
   // TODO: Write a test for this
   val moveEightOClock: RelativePositionRange =
