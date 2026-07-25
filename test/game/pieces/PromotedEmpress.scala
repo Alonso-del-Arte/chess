a07b9be60aeb58ff4d368bfb3191f332d4a00c31
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-24 20:10:52 -0400

    Passing hasSpecialMoves test

--- test/game/pieces/PromotedEmpress.scala
+++ test/game/pieces/PromotedEmpress.scala
@@ -4,8 +4,7 @@ import game.Player
 
 class PromotedEmpress(val pawn: Pawn) extends Empress with Promoted {
   override val affiliation: Player = pawn.affiliation
-  // TODO: Write tests for this
-  override val hasSpecialMoves: Boolean = true
+  override val hasSpecialMoves: Boolean = false
 
   // TODO: Write tests for this
   override def prior: Pawn = NeutralPawn
