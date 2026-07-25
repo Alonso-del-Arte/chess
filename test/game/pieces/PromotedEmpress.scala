187c56e2461ded2e1fe4cabf1f21841e299321c2
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-24 20:11:27 -0400

    Refactored hasSpecialMoves by deleting override

--- test/game/pieces/PromotedEmpress.scala
+++ test/game/pieces/PromotedEmpress.scala
@@ -4,7 +4,6 @@ import game.Player
 
 class PromotedEmpress(val pawn: Pawn) extends Empress with Promoted {
   override val affiliation: Player = pawn.affiliation
-  override val hasSpecialMoves: Boolean = false
 
   // TODO: Write tests for this
   override def prior: Pawn = NeutralPawn
