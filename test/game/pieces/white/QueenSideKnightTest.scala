7242ece707d3b80bf6385a2f7e31a91c8a674085
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-28 01:19:21 -0400

    New test class

---
+++ test/game/pieces/white/QueenSideKnightTest.scala
@@ -0,0 +1,10 @@
+package game.pieces.white
+
+import game.{Neutral, Player, RegularPosition, RelativePosition, White}
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class QueenSideKnightTest {
+
+}
