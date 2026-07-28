8adecd908390058eaef9584925b36e70e4a63651
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-28 15:53:12 -0400

    Failing possible captures test

--- test/game/pieces/white/QueenSideKnightTest.scala
+++ test/game/pieces/white/QueenSideKnightTest.scala
@@ -32,4 +32,11 @@ class QueenSideKnightTest {
     assert(QueenSideKnight.captureSameAsMove, "Knight captures same as move")
   }
 
+  @Test def testPossibleCaptures(): Unit = {
+    println("possibleCaptures")
+    val expected = QueenSideKnight.possibleMoves
+    val actual = QueenSideKnight.possibleCaptures
+    assertEquals(expected, actual)
+  }
+
 }
