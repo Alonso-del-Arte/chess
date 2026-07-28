6a9676635d48b8bafd191844c91a824a234fb8d0
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-28 00:50:43 -0400

    Back to passing can jump over test

--- src/game/pieces/Knight.scala
+++ src/game/pieces/Knight.scala
@@ -45,8 +45,7 @@ object Knight {
 abstract class Knight extends Piece {
   // TODO: Write tests for this
   override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
-  // TODO: Write tests for this
-  override val canJumpOver: Boolean = false
+  override val canJumpOver: Boolean = true
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
