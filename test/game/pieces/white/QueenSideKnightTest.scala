39d033380fd91b50dd5fe5baa144584f330b18ca
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-28 01:21:58 -0400

    Failing affiliation test

--- test/game/pieces/white/QueenSideKnightTest.scala
+++ test/game/pieces/white/QueenSideKnightTest.scala
@@ -1,10 +1,17 @@
 package game.pieces.white
 
-import game.{Neutral, Player, RegularPosition, RelativePosition, White}
+import game.{RegularPosition, RelativePosition, White}
 
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
 class QueenSideKnightTest {
+
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    val expected = White
+    val actual = QueenSideKnight.affiliation
+    assertEquals(expected, actual)
+  }
 
 }
