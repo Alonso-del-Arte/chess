aa723beed710e150b9e221626b1f6b120463dde0
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-30 14:22:11 -0400

    New test class

---
+++ test/game/pieces/black/KingSideKnightTest.scala
@@ -0,0 +1,11 @@
+package game.pieces.black
+
+import game.Black
+import game.pieces.Knight
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class KingSideKnightTest {
+
+}
