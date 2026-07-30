948df65503d0afbb4e3159a437bef9488c79d55f
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-30 14:27:42 -0400

    Refactored

--- src/game/pieces/black/QueenSideKnight.scala
+++ src/game/pieces/black/QueenSideKnight.scala
@@ -1,10 +1,9 @@
 package game.pieces.black
 
-import game.{Black, Player, RelativePositionRange}
-import game.pieces.{Bishop, Knight, Rook}
+import game.RelativePositionRange
+import game.pieces.{Knight, Rook}
 
 object QueenSideKnight extends Knight with BlackPiece {
-  override val canJumpOver: Boolean = true
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
