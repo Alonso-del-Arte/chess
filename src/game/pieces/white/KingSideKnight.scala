e774cc4a72f6d45d4b524f23ef9936ed47fd7bd8
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-30 14:03:40 -0400

    Passing hasSpecialMoves test

--- src/game/pieces/white/KingSideKnight.scala
+++ src/game/pieces/white/KingSideKnight.scala
@@ -3,7 +3,6 @@ package game.pieces.white
 import game.pieces.Knight
 
 object KingSideKnight extends Knight with WhitePiece {
-  // TODO: Write tests for this
-  override val hasSpecialMoves: Boolean = true
+  override val hasSpecialMoves: Boolean = false
 
 }
