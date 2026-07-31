a46ae0149d3e02dbb0db45f939530f86fa581236
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-30 23:40:07 -0400

    Refactored

--- src/game/pieces/black/QueenSideKnight.scala
+++ src/game/pieces/black/QueenSideKnight.scala
@@ -1,9 +1,5 @@
 package game.pieces.black
 
-import game.RelativePositionRange
 import game.pieces.Knight
 
-object QueenSideKnight extends Knight with BlackPiece {
-  override val hasSpecialMoves: Boolean = false
-
-}
+object QueenSideKnight extends Knight with BlackPiece
