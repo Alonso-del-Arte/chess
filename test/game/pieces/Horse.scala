f0bfbcc612855140fbd1e3356eeb0f06792e07f7
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-31 13:51:28 -0400

    Rewind to failing

--- test/game/pieces/Horse.scala
+++ test/game/pieces/Horse.scala
@@ -8,7 +8,15 @@ import game.RelativePositionRange
  * @author Alonso del Arte
  */
 abstract class Horse extends Piece {
-  override val possibleMoves: Set[RelativePositionRange] = Set()
-  override val canJumpOver: Boolean = true
+  // TODO: Write tests for this
+  override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
+  // TODO: Write tests for this
+  override val canJumpOver: Boolean = false
+  // TODO: Write tests for this
+  override val captureSameAsMove: Boolean = false
+  // TODO: Write tests for this
+  override val possibleCaptures: Set[RelativePositionRange] = Rook.moves
+  // TODO: Write tests for this
+  override val hasSpecialMoves: Boolean = true
 
 }
