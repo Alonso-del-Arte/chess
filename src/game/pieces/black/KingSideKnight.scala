b143c5562c5e8a8cababc680b42648a3971b261a
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-31 13:10:13 -0400

    Passing can jump over test

--- src/game/pieces/black/KingSideKnight.scala
+++ src/game/pieces/black/KingSideKnight.scala
@@ -4,8 +4,7 @@ import game.RelativePositionRange
 import game.pieces.{Knight, Rook}
 
 object KingSideKnight extends Knight with BlackPiece {
-  // TODO: Write tests for this
-  override val canJumpOver: Boolean = false
+  override val canJumpOver: Boolean = true
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
