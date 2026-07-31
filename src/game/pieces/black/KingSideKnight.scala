6ccfa528653759fac1c6d0072d9892cc2370e146
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-31 13:10:48 -0400

    Refactored can jump over by deleting override

--- src/game/pieces/black/KingSideKnight.scala
+++ src/game/pieces/black/KingSideKnight.scala
@@ -4,7 +4,6 @@ import game.RelativePositionRange
 import game.pieces.{Knight, Rook}
 
 object KingSideKnight extends Knight with BlackPiece {
-  override val canJumpOver: Boolean = true
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
