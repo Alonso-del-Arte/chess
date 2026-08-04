cf76afaec3e8e2481cc333927efee3aca2331f29
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-04 19:18:43 -0400

    Refactored as a direct subclass of Knight

--- test/game/pieces/Horse.scala
+++ test/game/pieces/Horse.scala
@@ -1,17 +1,8 @@
 package game.pieces
 
-import game.RelativePositionRange
-
 /**
  * The horse is a chess piece to be used strictly for testing purposes only. The
  * horse has the same moves as the knight.
  * @author Alonso del Arte
  */
-abstract class Horse extends Piece {
-  override val possibleMoves: Set[RelativePositionRange] = Knight.moves
-  override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = true
-  override val possibleCaptures: Set[RelativePositionRange] = Knight.moves
-  override val hasSpecialMoves: Boolean = false
-
-}
+abstract class Horse extends Knight
