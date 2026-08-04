2f19a1eaac8cbcc14b7aa8254077f9d2835f2350
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-04 19:26:40 -0400

    Passing affiliation test

--- test/game/pieces/darkgray/Horse.scala
+++ test/game/pieces/darkgray/Horse.scala
@@ -1,19 +1,18 @@
 package game.pieces.darkgray
 
-import game.{Neutral, Player, RelativePositionRange}
+import game.{DarkGray, Player, RelativePositionRange}
 import game.pieces.{Bishop, Rook}
 
 object Horse extends game.pieces.Horse with DarkGrayPiece {
-  // TODO: Write a test for this
-  override val affiliation: Player = Neutral
+  override val affiliation: Player = DarkGray
   // TODO: Write tests for this
-  override val possibleMoves: Set[RelativePositionRange] = Bishop.moves
+  override val possibleMoves: Set[RelativePositionRange] = Rook.moves
   // TODO: Write tests for this
   override val canJumpOver: Boolean = false
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
-  override val possibleCaptures: Set[RelativePositionRange] = Rook.moves
+  override val possibleCaptures: Set[RelativePositionRange] = Bishop.moves
   // TODO: Write tests for this
   override val hasSpecialMoves: Boolean = true
 
