21b6d58dd46915b3d44951485d9fe39f3a8f02fc
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-04 19:21:24 -0400

    New test class

---
+++ test/game/pieces/darkgray/HorseTest.scala
@@ -0,0 +1,10 @@
+package game.pieces.darkgray
+
+import game.{DarkGray, Neutral, Player, RegularPosition, RelativePosition}
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class HorseTest {
+
+}
