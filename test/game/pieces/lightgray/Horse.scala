c8a2b644914c315b75ffd3b642bd71c9088c370f
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-05 00:40:33 -0400

    Passing can jump over test

--- test/game/pieces/lightgray/Horse.scala
+++ test/game/pieces/lightgray/Horse.scala
@@ -6,8 +6,7 @@ import game.pieces.{Bishop, Knight}
 object Horse extends game.pieces.Horse with LightGrayPiece {
   override val affiliation: Player = LightGray
   override val possibleMoves: Set[RelativePositionRange] = Knight.moves
-  // TODO: Write tests for this
-  override val canJumpOver: Boolean = false
+  override val canJumpOver: Boolean = true
   // TODO: Write tests for this
   override val captureSameAsMove: Boolean = false
   // TODO: Write tests for this
