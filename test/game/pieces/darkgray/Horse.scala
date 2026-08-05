04efbdf7497effc5c21ce06d0c885ae922447efd
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-05 00:29:18 -0400

    Refactored

--- test/game/pieces/darkgray/Horse.scala
+++ test/game/pieces/darkgray/Horse.scala
@@ -1,14 +1,3 @@
 package game.pieces.darkgray
 
-import game.{DarkGray, Player, RelativePositionRange}
-import game.pieces.{Bishop, Knight}
-
-object Horse extends game.pieces.Horse with DarkGrayPiece {
-  override val affiliation: Player = DarkGray
-  override val possibleMoves: Set[RelativePositionRange] = Knight.moves
-  override val canJumpOver: Boolean = true
-  override val captureSameAsMove: Boolean = true
-  override val possibleCaptures: Set[RelativePositionRange] = Knight.moves
-  override val hasSpecialMoves: Boolean = false
-
-}
+object Horse extends game.pieces.Horse with DarkGrayPiece
