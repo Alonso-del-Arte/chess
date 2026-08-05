c4461d9c5e5eb3472b85e309950a7d1323afb389
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-04 23:54:41 -0400

    Passing possible moves test

--- test/game/pieces/darkgray/Horse.scala
+++ test/game/pieces/darkgray/Horse.scala
@@ -1,12 +1,11 @@
 package game.pieces.darkgray
 
 import game.{DarkGray, Player, RelativePositionRange}
-import game.pieces.{Bishop, Rook}
+import game.pieces.{Bishop, Knight}
 
 object Horse extends game.pieces.Horse with DarkGrayPiece {
   override val affiliation: Player = DarkGray
-  // TODO: Write tests for this
-  override val possibleMoves: Set[RelativePositionRange] = Rook.moves
+  override val possibleMoves: Set[RelativePositionRange] = Knight.moves
   // TODO: Write tests for this
   override val canJumpOver: Boolean = false
   // TODO: Write tests for this
