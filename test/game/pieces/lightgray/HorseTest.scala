765a332899ab5f4c2aae478bb45a861338735e0f
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-05 00:33:17 -0400

    New test class

---
+++ test/game/pieces/lightgray/HorseTest.scala
@@ -0,0 +1,11 @@
+package game.pieces.lightgray
+
+import game.LightGray
+import game.pieces.Knight
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class HorseTest {
+
+}
