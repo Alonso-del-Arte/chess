1c7ce3b57b892f81bbeb78236fc03bf77d9fc727
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-05 23:59:04 -0400

    Refactored can jump over by deleting override

--- test/game/pieces/lightgray/Horse.scala
+++ test/game/pieces/lightgray/Horse.scala
@@ -4,7 +4,6 @@ import game.RelativePositionRange
 import game.pieces.Knight
 
 object Horse extends game.pieces.Horse with LightGrayPiece {
-  override val canJumpOver: Boolean = true
   override val captureSameAsMove: Boolean = true
   override val possibleCaptures: Set[RelativePositionRange] = Knight.moves
   override val hasSpecialMoves: Boolean = false
