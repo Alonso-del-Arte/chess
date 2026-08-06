bdc8743647add4ad7ca8d55f3015e78450cb8eda
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-06 00:15:42 -0400

    Passing has special moves test

--- src/game/pieces/Pawn.scala
+++ src/game/pieces/Pawn.scala
@@ -2,7 +2,6 @@ package game.pieces
 
 abstract class Pawn extends Piece {
   override val captureSameAsMove: Boolean = false
-  // TODO: Write a test for this
-  override val hasSpecialMoves: Boolean = false
+  override val hasSpecialMoves: Boolean = true
 
 }
