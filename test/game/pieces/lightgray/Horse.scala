a893414279e3a01bcc72490e0b37ee1bcf6b0051
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-05 23:58:03 -0400

    Refactored affiliation by deleting override

--- test/game/pieces/lightgray/Horse.scala
+++ test/game/pieces/lightgray/Horse.scala
@@ -1,10 +1,9 @@
 package game.pieces.lightgray
 
-import game.{LightGray, Player, RelativePositionRange}
+import game.RelativePositionRange
 import game.pieces.Knight
 
 object Horse extends game.pieces.Horse with LightGrayPiece {
-  override val affiliation: Player = LightGray
   override val possibleMoves: Set[RelativePositionRange] = Knight.moves
   override val canJumpOver: Boolean = true
   override val captureSameAsMove: Boolean = true
