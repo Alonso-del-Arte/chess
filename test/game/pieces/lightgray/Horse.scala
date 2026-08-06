948cba295fc8dcadfad08c7e6b1af55f4068f7fc
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-05 23:52:30 -0400

    Passing capture same as move test

--- test/game/pieces/lightgray/Horse.scala
+++ test/game/pieces/lightgray/Horse.scala
@@ -7,8 +7,7 @@ object Horse extends game.pieces.Horse with LightGrayPiece {
   override val affiliation: Player = LightGray
   override val possibleMoves: Set[RelativePositionRange] = Knight.moves
   override val canJumpOver: Boolean = true
-  // TODO: Write tests for this
-  override val captureSameAsMove: Boolean = false
+  override val captureSameAsMove: Boolean = true
   // TODO: Write tests for this
   override val possibleCaptures: Set[RelativePositionRange] = Bishop.moves
   // TODO: Write tests for this
