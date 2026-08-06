4522950cdb84ed15986b22d7e42f97bf89ba8848
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-05 23:59:34 -0400

    Refactored possible captures by deleting override

--- test/game/pieces/lightgray/Horse.scala
+++ test/game/pieces/lightgray/Horse.scala
@@ -4,7 +4,6 @@ import game.RelativePositionRange
 import game.pieces.Knight
 
 object Horse extends game.pieces.Horse with LightGrayPiece {
-  override val captureSameAsMove: Boolean = true
   override val possibleCaptures: Set[RelativePositionRange] = Knight.moves
   override val hasSpecialMoves: Boolean = false
 
