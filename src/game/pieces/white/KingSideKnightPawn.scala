dd4ca4273daa18255284ed370b423b97b7c6a568
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-10 17:29:42 -0400

    Refactored possible moves by deleting override

--- src/game/pieces/white/KingSideKnightPawn.scala
+++ src/game/pieces/white/KingSideKnightPawn.scala
@@ -3,9 +3,6 @@ package game.pieces.white
 import game.{RelativePosition, RelativePositionRange}
 
 object KingSideKnightPawn extends WhitePawn {
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, 1),
-      new RelativePosition(0, 2)))
   // TODO: Write a test for this
   override val canJumpOver: Boolean = true
   // TODO: Write a test for this
