e7cc097d42ce22e40f4ee18ecbc7712d531fe855
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-10 18:54:09 -0400

    Back to passing possible captures test

--- src/game/pieces/white/KingSideKnightPawn.scala
+++ src/game/pieces/white/KingSideKnightPawn.scala
@@ -5,9 +5,9 @@ import game.{RelativePosition, RelativePositionRange}
 object KingSideKnightPawn extends WhitePawn {
   override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = false
-  // TODO: Write a test for this
   override val possibleCaptures: Set[RelativePositionRange] =
-    KingSideKnight.possibleCaptures
+    Set(RelativePositionRange(new RelativePosition(-1, 1)),
+      RelativePositionRange(new RelativePosition(1, 1)))
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = false
 
