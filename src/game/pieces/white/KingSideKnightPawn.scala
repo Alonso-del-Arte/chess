e3f93043ef37f20dd30b441990dd26a7559ca5ff
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-10 19:10:37 -0400

    Documentation

--- src/game/pieces/white/KingSideKnightPawn.scala
+++ src/game/pieces/white/KingSideKnightPawn.scala
@@ -1,3 +1,8 @@
 package game.pieces.white
 
-object KingSideKnightPawn extends WhitePawn 
+/**
+ * White's kingside knight's pawn starts out on g2. If this pawn doesn't move
+ * and White castles, this pawn protects the king from attacks from the Black
+ * queen or either of the Black rooks.
+ */
+object KingSideKnightPawn extends WhitePawn
