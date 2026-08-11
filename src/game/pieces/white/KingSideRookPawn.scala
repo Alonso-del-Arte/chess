1a66b98b6f791b0678ba5804a3a42a469d77c59d
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-10 23:46:54 -0400

    Rewind to failing

--- src/game/pieces/white/KingSideRookPawn.scala
+++ src/game/pieces/white/KingSideRookPawn.scala
@@ -1,6 +1,20 @@
 package game.pieces.white
 
+import game.{Neutral, Player, RelativePositionRange}
+
 object KingSideRookPawn extends WhitePawn {
+  // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
+  override val possibleMoves: Set[RelativePositionRange] =
+    KingSideKnight.possibleMoves
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
+  // TODO: Write a test for this
+  override val possibleCaptures: Set[RelativePositionRange] =
+    KingSideKnight.possibleCaptures
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = false
 
