66672882b05038ade8e296a60c59cccb6a90ac80
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-11 00:56:09 -0400

    Rewind to failing

--- src/game/pieces/white/QueenPawn.scala
+++ src/game/pieces/white/QueenPawn.scala
@@ -1,11 +1,25 @@
 package game.pieces.white
 
+import game.{Neutral, Player, RelativePositionRange}
+
 /**
  * The pawn in front of the queen at the beginning of the game. Not to be
  * confused with [[game.pieces.PromotedQueen]], a pawn that is promoted to a
  * queen.
  */
 object QueenPawn extends WhitePawn {
+  // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
+  override val possibleMoves: Set[RelativePositionRange] =
+    KingSideKnight.possibleMoves
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
+  // TODO: Write a test for this
+  override val possibleCaptures: Set[RelativePositionRange] =
+    KingSideKnight.possibleCaptures
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = false
 
