fe26a02d2cb8610052294353f3404b352a9a1e6d
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-11 00:31:36 -0400

    Failing has special moves test

--- test/game/pieces/white/KingSideRookPawnTest.scala
+++ test/game/pieces/white/KingSideRookPawnTest.scala
@@ -42,4 +42,10 @@ class KingSideRookPawnTest {
     assertEquals(expected, actual)
   }
 
+  @Test def testHasSpecialMoves(): Unit = {
+    println("hasSpecialMoves")
+    val msg = "Pawn should have special moves"
+    assert(KingSideRookPawn.hasSpecialMoves, msg)
+  }
+
 }
