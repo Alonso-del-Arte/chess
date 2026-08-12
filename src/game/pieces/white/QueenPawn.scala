ffc1958ee961342817265796f24ce9228b769fb2
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-12 01:06:00 -0400

    Refactored possible captures by deleting override

--- src/game/pieces/white/QueenPawn.scala
+++ src/game/pieces/white/QueenPawn.scala
@@ -1,16 +1,11 @@
 package game.pieces.white
 
-import game.{RelativePosition, RelativePositionRange}
-
 /**
  * The pawn in front of the queen at the beginning of the game. Not to be
  * confused with [[game.pieces.PromotedQueen]], a pawn that is promoted to a
  * queen.
  */
 object QueenPawn extends WhitePawn {
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(-1, 1)),
-      RelativePositionRange(new RelativePosition(1, 1)))
   override val hasSpecialMoves: Boolean = true
 
 }
