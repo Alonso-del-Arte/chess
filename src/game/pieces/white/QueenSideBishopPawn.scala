f1e433a58cf2843a59796a5f2dbc0b5399d15848
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-12 23:59:22 -0400

    Refactored affiliation by deleting override

--- src/game/pieces/white/QueenSideBishopPawn.scala
+++ src/game/pieces/white/QueenSideBishopPawn.scala
@@ -3,7 +3,6 @@ package game.pieces.white
 import game.{Player, RelativePosition, RelativePositionRange, White}
 
 object QueenSideBishopPawn extends WhitePawn {
-  override val affiliation: Player = White
   override val possibleMoves: Set[RelativePositionRange] =
     Set(new RelativePositionRange(new RelativePosition(0, 1),
       new RelativePosition(0, 2)))
