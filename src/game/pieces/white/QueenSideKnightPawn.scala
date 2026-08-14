1f2ad34221925c812f639991f3d2e69a8557a461
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-14 00:01:11 -0400

    Removed unused imports

--- src/game/pieces/white/QueenSideKnightPawn.scala
+++ src/game/pieces/white/QueenSideKnightPawn.scala
@@ -1,7 +1,5 @@
 package game.pieces.white
 
-import game.{Player, RelativePosition, RelativePositionRange, White}
-
 object QueenSideKnightPawn extends WhitePawn {
   override val hasSpecialMoves: Boolean = true
 
