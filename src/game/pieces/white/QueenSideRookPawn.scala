9c3b24829e39141e7dc26f501817ea8babb094e3
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-14 00:05:51 -0400

    Back to passing affiliation test

--- src/game/pieces/white/QueenSideRookPawn.scala
+++ src/game/pieces/white/QueenSideRookPawn.scala
@@ -1,10 +1,9 @@
 package game.pieces.white
 
-import game.{Neutral, Player, RelativePosition, RelativePositionRange}
+import game.{Player, RelativePosition, RelativePositionRange, White}
 
 object QueenSideRookPawn extends WhitePawn {
-  // TODO: Write a test for this
-  override val affiliation: Player = Neutral
+  override val affiliation: Player = White
   // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] =
     KingSideKnight.possibleMoves
