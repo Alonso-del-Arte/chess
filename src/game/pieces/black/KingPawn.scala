a102d20bf7cd4b3feecd665251b912abe9358a45
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-18 01:11:03 -0400

    Rewind to failing all tests

--- src/game/pieces/black/KingPawn.scala
+++ src/game/pieces/black/KingPawn.scala
@@ -1,6 +1,20 @@
 package game.pieces.black
 
+import game.{Neutral, Player, RelativePosition, RelativePositionRange}
+
 object KingPawn extends BlackPawn with BlackPiece {
+  // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
+  override val possibleMoves: Set[RelativePositionRange] =
+    KingSideKnight.possibleMoves
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
+  // TODO: Write a test for this
+  override val possibleCaptures: Set[RelativePositionRange] =
+    KingSideKnight.possibleCaptures
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = false
 
