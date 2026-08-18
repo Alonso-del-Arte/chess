d5bf0f0b1311e4e846d4b53f43426af17e2c2981
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-18 01:06:35 -0400

    Refactored canJumpOver by deleting override

--- src/game/pieces/white/QueenSideRookPawn.scala
+++ src/game/pieces/white/QueenSideRookPawn.scala
@@ -3,7 +3,6 @@ package game.pieces.white
 import game.{RelativePosition, RelativePositionRange}
 
 object QueenSideRookPawn extends WhitePawn {
-  override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = false
   override val possibleCaptures: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(-1, 1)),
