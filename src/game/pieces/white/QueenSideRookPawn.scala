774d613a8ce43fd495d4bca45034d98934e3d74d
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-18 01:07:02 -0400

    Refactored captureSameAsMove by deleting override

--- src/game/pieces/white/QueenSideRookPawn.scala
+++ src/game/pieces/white/QueenSideRookPawn.scala
@@ -3,7 +3,6 @@ package game.pieces.white
 import game.{RelativePosition, RelativePositionRange}
 
 object QueenSideRookPawn extends WhitePawn {
-  override val captureSameAsMove: Boolean = false
   override val possibleCaptures: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(-1, 1)),
       RelativePositionRange(new RelativePosition(1, 1)))
