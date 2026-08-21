48d9ad0f343dd849551090cfe2d2d91444d83ff4
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-21 01:13:30 -0400

    Passing has special moves test

--- src/game/pieces/black/KingSideRookPawn.scala
+++ src/game/pieces/black/KingSideRookPawn.scala
@@ -12,7 +12,6 @@ object KingSideRookPawn extends BlackPawn with BlackPiece {
   override val possibleCaptures: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(-1, -1)),
       RelativePositionRange(new RelativePosition(1, -1)))
-  // TODO: Write a test for this
-  override val hasSpecialMoves: Boolean = false
+  override val hasSpecialMoves: Boolean = true
 
 }
