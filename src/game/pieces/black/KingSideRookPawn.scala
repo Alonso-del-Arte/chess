63bfe8ac2694fe11f7ccabc57dcb740415823521
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-21 01:16:40 -0400

    Refactored possible captures by deleting override

--- src/game/pieces/black/KingSideRookPawn.scala
+++ src/game/pieces/black/KingSideRookPawn.scala
@@ -1,11 +1,6 @@
 package game.pieces.black
 
-import game.{RelativePosition, RelativePositionRange}
-
 object KingSideRookPawn extends BlackPawn with BlackPiece {
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(-1, -1)),
-      RelativePositionRange(new RelativePosition(1, -1)))
   override val hasSpecialMoves: Boolean = true
 
 }
