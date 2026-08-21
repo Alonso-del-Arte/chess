51bdb4d806ccc99dbabae61e7afb628f519c3856
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-21 01:19:13 -0400

    Refactored has special moves by deleting override

--- src/game/pieces/black/KingSideRookPawn.scala
+++ src/game/pieces/black/KingSideRookPawn.scala
@@ -1,6 +1,3 @@
 package game.pieces.black
 
-object KingSideRookPawn extends BlackPawn with BlackPiece {
-  override val hasSpecialMoves: Boolean = true
-
-}
+object KingSideRookPawn extends BlackPawn with BlackPiece
