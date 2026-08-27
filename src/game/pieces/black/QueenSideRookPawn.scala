74e16f7a1f4f07fbd15518c02e5c8713a0d9e706
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-27 00:57:34 -0400

    Refactored

--- src/game/pieces/black/QueenSideRookPawn.scala
+++ src/game/pieces/black/QueenSideRookPawn.scala
@@ -1,6 +1,3 @@
 package game.pieces.black
 
-object QueenSideRookPawn extends BlackPawn with BlackPiece {
-  override val hasSpecialMoves: Boolean = true
-
-}
+object QueenSideRookPawn extends BlackPawn with BlackPiece
