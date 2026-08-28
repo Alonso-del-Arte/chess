a6826ca78bad8239557a82daa2bafdcca6071ba6
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-28 15:54:22 -0400

    Failing affiliation test

--- test/game/pieces/darkgray/EmperorsPawnTest.scala
+++ test/game/pieces/darkgray/EmperorsPawnTest.scala
@@ -7,4 +7,9 @@ import org.junit.jupiter.api.Assertions._
 
 class EmperorsPawnTest {
 
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    assertEquals(DarkGray, EmperorsPawn.affiliation)
+  }
+
 }
