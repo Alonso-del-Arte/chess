cebf86ffc0f2cb47b79069ba65c198311396d260
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-28 00:14:57 -0400

    Failing affiliation test

--- test/game/pieces/darkgray/DarkGrayPawnTest.scala
+++ test/game/pieces/darkgray/DarkGrayPawnTest.scala
@@ -1,14 +1,15 @@
 package game.pieces.darkgray
 
-import game.{Black, RelativePosition, RelativePositionRange}
+import game.{DarkGray, RelativePosition, RelativePositionRange}
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
 
 class DarkGrayPawnTest {
 
-  @Test def placeholder(): Unit = {
-    fail("PLACEHOLDER")
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    assertEquals(DarkGray, DarkGrayPawnImpl.affiliation)
   }
 
   private object DarkGrayPawnImpl extends DarkGrayPawn
