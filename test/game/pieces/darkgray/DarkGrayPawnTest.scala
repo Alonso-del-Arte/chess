9da0767f7e412292aadebefac5797940c7296073
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-28 00:20:17 -0400

    Failing possible moves test

--- test/game/pieces/darkgray/DarkGrayPawnTest.scala
+++ test/game/pieces/darkgray/DarkGrayPawnTest.scala
@@ -12,6 +12,15 @@ class DarkGrayPawnTest {
     assertEquals(DarkGray, DarkGrayPawnImpl.affiliation)
   }
 
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val start = new RelativePosition(0, -1)
+    val finish = new RelativePosition(0, -2)
+    val expected = Set(new RelativePositionRange(start, finish))
+    val actual = DarkGrayPawnImpl.possibleMoves
+    assertEquals(expected, actual)
+  }
+
   private object DarkGrayPawnImpl extends DarkGrayPawn
 
 }
