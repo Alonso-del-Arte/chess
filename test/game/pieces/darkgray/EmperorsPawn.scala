acb53977e838aead550219de1dd63650878ca41d
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-28 15:56:45 -0400

    Note for a potential later refactoring

--- test/game/pieces/darkgray/EmperorsPawn.scala
+++ test/game/pieces/darkgray/EmperorsPawn.scala
@@ -2,6 +2,7 @@ package game.pieces.darkgray
 
 import game.{DarkGray, Player, RelativePosition, RelativePositionRange}
 
+// TODO: Determine if it's really necessary to implement DarkGrayPiece directly
 object EmperorsPawn extends DarkGrayPawn with DarkGrayPiece {
   override val affiliation: Player = DarkGray
   // TODO: Write a test for this
