45027c23c9c6b783377d82262867c0bf3b1d9854
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-27 23:53:46 -0400

    Made abstract

--- test/game/pieces/darkgray/DarkGrayPawn.scala
+++ test/game/pieces/darkgray/DarkGrayPawn.scala
@@ -3,7 +3,7 @@ package game.pieces.darkgray
 import game.{RelativePosition, RelativePositionRange}
 import game.pieces.Pawn
 
-class DarkGrayPawn extends Pawn with DarkGrayPiece {
+abstract class DarkGrayPawn extends Pawn with DarkGrayPiece {
   override val possibleMoves: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(0, -1)))
 
--- test/game/pieces/lightgray/LightGrayPawn.scala
+++ test/game/pieces/lightgray/LightGrayPawn.scala
@@ -3,7 +3,7 @@ package game.pieces.lightgray
 import game.{RelativePosition, RelativePositionRange}
 import game.pieces.Pawn
 
-class LightGrayPawn extends Pawn with LightGrayPiece {
+abstract class LightGrayPawn extends Pawn with LightGrayPiece {
   override val possibleMoves: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(0, 1)))
 
