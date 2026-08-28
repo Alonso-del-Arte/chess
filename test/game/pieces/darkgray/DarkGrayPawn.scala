48af39a2e0271a8f7b396a62aa3feeae523244df
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-28 15:20:30 -0400

    Passing possible captures test

--- test/game/pieces/darkgray/DarkGrayPawn.scala
+++ test/game/pieces/darkgray/DarkGrayPawn.scala
@@ -10,9 +10,9 @@ abstract class DarkGrayPawn extends Pawn with DarkGrayPiece {
       new RelativePosition(0, -2)))
   override val canJumpOver: Boolean = false
   override val captureSameAsMove: Boolean = false
-  // TODO: Write a test for this
-  override def possibleCaptures: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(0, -10)))
+  override val possibleCaptures: Set[RelativePositionRange] =
+    Set(RelativePositionRange(new RelativePosition(-1, -1)),
+      RelativePositionRange(new RelativePosition(1, -1)))
   // TODO: Write a test for this
   override val hasSpecialMoves: Boolean = false
 
