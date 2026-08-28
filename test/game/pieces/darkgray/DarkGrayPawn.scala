6eafd98b338a6c0caea0dcc27173e2ce79affb67
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-27 23:56:32 -0400

    Preparing to fail the next few tests

--- test/game/pieces/darkgray/DarkGrayPawn.scala
+++ test/game/pieces/darkgray/DarkGrayPawn.scala
@@ -4,7 +4,13 @@ import game.{RelativePosition, RelativePositionRange}
 import game.pieces.Pawn
 
 abstract class DarkGrayPawn extends Pawn with DarkGrayPiece {
+  // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(0, -1)))
+    Set(RelativePositionRange(new RelativePosition(0, -10)))
+  // TODO: Write a test for this
+  override def possibleCaptures: Set[RelativePositionRange] =
+    Set(RelativePositionRange(new RelativePosition(0, -10)))
+  // TODO: Write a test for this
+  override val hasSpecialMoves: Boolean = false
 
 }
