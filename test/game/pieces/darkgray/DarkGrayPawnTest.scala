a2626c02ec6699bf4df317a92a0c2b9ccaaccf5e
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-28 00:03:06 -0400

    New test class

---
+++ test/game/pieces/darkgray/DarkGrayPawnTest.scala
@@ -0,0 +1,14 @@
+package game.pieces.darkgray
+
+import game.{Black, RelativePosition, RelativePositionRange}
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class DarkGrayPawnTest {
+
+  @Test def placeholder(): Unit = {
+    fail("PLACEHOLDER")
+  }
+
+}
---
+++ test/game/pieces/lightgray/LightGrayPawnTest.scala
@@ -0,0 +1,14 @@
+package game.pieces.lightgray
+
+import game.{Black, RelativePosition, RelativePositionRange}
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class LightGrayPawnTest {
+
+  @Test def placeholder(): Unit = {
+    fail("PLACEHOLDER")
+  }
+
+}
