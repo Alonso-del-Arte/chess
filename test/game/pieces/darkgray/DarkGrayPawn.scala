5126b43a88ee627cbd9003e0c38a0e4758561908
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-28 00:12:57 -0400

    More preparation to fail tests

--- test/game/pieces/darkgray/DarkGrayPawn.scala
+++ test/game/pieces/darkgray/DarkGrayPawn.scala
@@ -1,12 +1,18 @@
 package game.pieces.darkgray
 
-import game.{RelativePosition, RelativePositionRange}
+import game.{DarkGray, Neutral, Player, RelativePosition, RelativePositionRange}
 import game.pieces.Pawn
 
 abstract class DarkGrayPawn extends Pawn with DarkGrayPiece {
   // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(0, -10)))
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
   // TODO: Write a test for this
   override def possibleCaptures: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(0, -10)))
--- test/game/pieces/lightgray/LightGrayPawn.scala
+++ test/game/pieces/lightgray/LightGrayPawn.scala
@@ -1,12 +1,19 @@
 package game.pieces.lightgray
 
-import game.{RelativePosition, RelativePositionRange}
+import game.{LightGray, Neutral, Player, RelativePosition,
+  RelativePositionRange}
 import game.pieces.Pawn
 
 abstract class LightGrayPawn extends Pawn with LightGrayPiece {
   // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(0, -10)))
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
   // TODO: Write a test for this
   override def possibleCaptures: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(0, -10)))
