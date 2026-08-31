cc1f37ae0fa4e21b4d7d2a2b9325694a84844bad
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-31 14:59:24 -0400

    Refactored by deleting overrides

--- test/game/pieces/darkgray/EmperorsPawn.scala
+++ test/game/pieces/darkgray/EmperorsPawn.scala
@@ -3,16 +3,4 @@ package game.pieces.darkgray
 import game.{DarkGray, Player, RelativePosition, RelativePositionRange}
 
 // TODO: Determine if it's really necessary to implement DarkGrayPiece directly
-object EmperorsPawn extends DarkGrayPawn with DarkGrayPiece {
-  override val affiliation: Player = DarkGray
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, -1),
-      new RelativePosition(0, -2)))
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = false
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(-1, -1)),
-      RelativePositionRange(new RelativePosition(1, -1)))
-  override val hasSpecialMoves: Boolean = true
-
-}
+object EmperorsPawn extends DarkGrayPawn with DarkGrayPiece 
