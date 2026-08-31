86f9294232bdace3fc484160014f62d8576bde98
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-31 15:07:14 -0400

    New test class

---
+++ test/game/pieces/darkgray/EmpressesPawnTest.scala
@@ -0,0 +1,10 @@
+package game.pieces.darkgray
+
+import game.{DarkGray, RelativePosition, RelativePositionRange}
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class EmpressesPawnTest {
+
+}
