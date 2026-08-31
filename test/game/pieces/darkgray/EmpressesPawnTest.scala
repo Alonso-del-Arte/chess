a15380a35b280c710832893b46081d8c380fb2e5
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-31 15:09:32 -0400

    Failing possible moves test

--- test/game/pieces/darkgray/EmpressesPawnTest.scala
+++ test/game/pieces/darkgray/EmpressesPawnTest.scala
@@ -12,4 +12,13 @@ class EmpressesPawnTest {
     assertEquals(DarkGray, EmpressesPawn.affiliation)
   }
 
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val start = new RelativePosition(0, -1)
+    val finish = new RelativePosition(0, -2)
+    val expected = Set(new RelativePositionRange(start, finish))
+    val actual = EmpressesPawn.possibleMoves
+    assertEquals(expected, actual)
+  }
+
 }
