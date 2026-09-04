0db6989a51d39db4aad3f50940cadb6bf0f5838c
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-04 17:08:53 -0400

    Failing possible captures test

--- test/game/pieces/darkgray/HorsesPawnTest.scala
+++ test/game/pieces/darkgray/HorsesPawnTest.scala
@@ -33,4 +33,14 @@ class HorsesPawnTest {
     assert(!HorsesPawn.captureSameAsMove, msg)
   }
 
+  @Test def testPossibleCaptures(): Unit = {
+    println("possibleCaptures")
+    val captureLeft = new RelativePosition(-1, -1)
+    val captureRight = new RelativePosition(1, -1)
+    val expected = Set(RelativePositionRange(captureLeft),
+      RelativePositionRange(captureRight))
+    val actual = HorsesPawn.possibleCaptures
+    assertEquals(expected, actual)
+  }
+
 }
