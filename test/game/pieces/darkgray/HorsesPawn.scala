d089a4a6a5aa213652bed3a41d890682e501267f
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-04 17:11:06 -0400

    Passing has special moves test

--- test/game/pieces/darkgray/HorsesPawn.scala
+++ test/game/pieces/darkgray/HorsesPawn.scala
@@ -12,7 +12,6 @@ object HorsesPawn extends DarkGrayPawn with DarkGrayPiece {
   override val possibleCaptures: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(-1, -1)),
       RelativePositionRange(new RelativePosition(1, -1)))
-  // TODO: Write a test for this
-  override val hasSpecialMoves: Boolean = false
+  override val hasSpecialMoves: Boolean = true
 
 }
