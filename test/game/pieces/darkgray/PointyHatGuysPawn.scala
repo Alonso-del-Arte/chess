b570572c28663b75c1a6206eeabc48eabe733e3e
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-04 17:14:30 -0400

    Preparing to fail tests

--- test/game/pieces/darkgray/PointyHatGuysPawn.scala
+++ test/game/pieces/darkgray/PointyHatGuysPawn.scala
@@ -1,5 +1,20 @@
 package game.pieces.darkgray
 
+import game.{DarkGray, Neutral, Player, RelativePosition, RelativePositionRange}
+
 object PointyHatGuysPawn extends DarkGrayPawn with DarkGrayPiece {
+  // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
+  override val possibleMoves: Set[RelativePositionRange] = Horse.possibleMoves
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
+  // TODO: Write a test for this
+  override val possibleCaptures: Set[RelativePositionRange] =
+    Tower.possibleCaptures
+  // TODO: Write a test for this
+  override val hasSpecialMoves: Boolean = false
 
 }
