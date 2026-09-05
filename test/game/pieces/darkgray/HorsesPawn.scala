f8ddc017a22b76e28f63ad23051d8770a983f15c
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-04 23:45:46 -0400

    Explanation of horse's pawn

--- test/game/pieces/darkgray/HorsesPawn.scala
+++ test/game/pieces/darkgray/HorsesPawn.scala
@@ -1,3 +1,7 @@
 package game.pieces.darkgray
 
+/**
+ * The Dark Gray horse's pawn is equivalent to the Black kingside knight's pawn,
+ * and starts out on g7.
+ */
 object HorsesPawn extends DarkGrayPawn
