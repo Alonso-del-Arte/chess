89e9775b4afa7c0ececbd0cd265d335af9b7b13f
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-04 23:36:10 -0400

    Failing possible moves test

--- test/game/pieces/darkgray/PointyHatGuysPawnTest.scala
+++ test/game/pieces/darkgray/PointyHatGuysPawnTest.scala
@@ -12,4 +12,13 @@ class PointyHatGuysPawnTest {
     assertEquals(DarkGray, PointyHatGuysPawn.affiliation)
   }
 
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val start = new RelativePosition(0, -1)
+    val finish = new RelativePosition(0, -2)
+    val expected = Set(new RelativePositionRange(start, finish))
+    val actual = PointyHatGuysPawn.possibleMoves
+    assertEquals(expected, actual)
+  }
+
 }
