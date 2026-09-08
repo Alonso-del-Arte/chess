47ff7e399fb9d52f02a228493705626f2e3549c7
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-08 01:08:29 -0400

    Documentation

--- test/game/pieces/darkgray/PointyHatGuysPawn.scala
+++ test/game/pieces/darkgray/PointyHatGuysPawn.scala
@@ -1,3 +1,7 @@
 package game.pieces.darkgray
 
+/**
+ * The Dark Gray pointy hat guy's pawn is equivalent to the Black kingside
+ * bishop's pawn, starts out on f7.
+ */
 object PointyHatGuysPawn extends DarkGrayPawn
