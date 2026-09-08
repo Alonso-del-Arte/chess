556c868551fd3ff38a72a15a972eb2d6ce1748ad
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-08 01:00:21 -0400

    Passing canJumpOver test

--- test/game/pieces/darkgray/PointyHatGuysPawn.scala
+++ test/game/pieces/darkgray/PointyHatGuysPawn.scala
@@ -8,8 +8,7 @@ object PointyHatGuysPawn extends DarkGrayPawn with DarkGrayPiece {
     Set(new RelativePositionRange(new RelativePosition(0, -1),
       new RelativePosition(0, -2)))
   override val canJumpOver: Boolean = false
-  // TODO: Write a test for this
-  override val captureSameAsMove: Boolean = true
+  override val captureSameAsMove: Boolean = false
   // TODO: Write a test for this
   override val possibleCaptures: Set[RelativePositionRange] =
     Tower.possibleCaptures
