1c3fc99ca20a5710e06632fa5b1c262b5191cd4c
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-08 01:02:55 -0400

    Failing has special moves test

--- test/game/pieces/darkgray/PointyHatGuysPawnTest.scala
+++ test/game/pieces/darkgray/PointyHatGuysPawnTest.scala
@@ -43,4 +43,10 @@ class PointyHatGuysPawnTest {
     assertEquals(expected, actual)
   }
 
+  @Test def testHasSpecialMoves(): Unit = {
+    println("hasSpecialMoves")
+    val msg = "Pawn should have special moves"
+    assert(PointyHatGuysPawn.hasSpecialMoves, msg)
+  }
+
 }
