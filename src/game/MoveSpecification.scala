a386e0b8db4cc2f948a17e0c41b5661f37ae537c
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-08 22:55:03 -0400

    Starting to rework

--- src/game/MoveSpecification.scala
+++ src/game/MoveSpecification.scala
@@ -2,21 +2,19 @@ package game
 
 import game.pieces.Piece
 
-// TODO: Reevaluate parameters: is start necessary? should destination be
-//  different type?
 /**
- * Specifies one possible move for a piece, and whether or not there are special
+ * Specifies one possible move for a piece, and whether there are special
  * prerequisites for the move (e.g., castling, en passant). It should be
  * understood that the move described by this instance might not always be
  * available, such as because it would put the piece off the board, or because
  * the destination is occupied by a piece of the same side.
  * @param piece The piece which is to be moved. For example, a knight.
- * @param start The starting position.
  * @param destination The destination. For example two ranks up, a file to the
  *                    left.
- * @param prerequisites An `Option` with the special prerequisites for this
- *                      move. Empty by default.
+ * @param prerequisites A function that indicates whether a given board state meets the prerequisites for the move.
+ * @param isCapture Whether the move is a capture.
  */
-class MoveSpecification(val piece: Piece, val start: Position,
-                        val destination: Position,
-                        val prerequisites: Option[BoardState => Boolean] = None)
+abstract class MoveSpecification(val piece: Piece,
+                                 val destination: RelativePositionRange,
+                                 val prerequisites: BoardState => Boolean,
+                                 val isCapture: Boolean = true)
