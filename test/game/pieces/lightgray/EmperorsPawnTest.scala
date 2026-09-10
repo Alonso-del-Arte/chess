3929d57dd0883a9f7712fd79a47222904452ab98
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-10 00:54:19 -0400

    Failing affiliation test

--- test/game/pieces/lightgray/EmperorsPawnTest.scala
+++ test/game/pieces/lightgray/EmperorsPawnTest.scala
@@ -1,10 +1,15 @@
 package game.pieces.lightgray
 
-import game.{LightGray, RelativePosition, RelativePositionRange}
+import game.{DarkGray, LightGray, RelativePosition, RelativePositionRange}
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
 
 class EmperorsPawnTest {
+
+  @Test def testAffiliation(): Unit = {
+    println("affiliation")
+    assertEquals(LightGray, EmperorsPawn.affiliation)
+  }
 
 }
