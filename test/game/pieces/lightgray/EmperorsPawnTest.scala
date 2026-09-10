39b61b56975f3500f9d29703fe66edfab7f3ae77
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-10 00:30:48 -0400

    New test class

---
+++ test/game/pieces/lightgray/EmperorsPawnTest.scala
@@ -0,0 +1,10 @@
+package game.pieces.lightgray
+
+import game.{LightGray, RelativePosition, RelativePositionRange}
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class EmperorsPawnTest {
+
+}
