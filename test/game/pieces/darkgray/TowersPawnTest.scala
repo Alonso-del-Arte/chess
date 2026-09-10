a1048ff89ebc90fb2f6a1bbce2f9bbda3536f95a
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-09 23:51:24 -0400

    Failing possible moves test

--- test/game/pieces/darkgray/TowersPawnTest.scala
+++ test/game/pieces/darkgray/TowersPawnTest.scala
@@ -1,6 +1,6 @@
 package game.pieces.darkgray
 
-import game.DarkGray
+import game.{DarkGray, RelativePosition, RelativePositionRange}
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
@@ -10,6 +10,15 @@ class TowersPawnTest {
   @Test def testAffiliation(): Unit = {
     println("affiliation")
     assertEquals(DarkGray, TowersPawn.affiliation)
+  }
+
+  @Test def testPossibleMoves(): Unit = {
+    println("possibleMoves")
+    val start = new RelativePosition(0, -1)
+    val finish = new RelativePosition(0, -2)
+    val expected = Set(new RelativePositionRange(start, finish))
+    val actual = TowersPawn.possibleMoves
+    assertEquals(expected, actual)
   }
 
 }
