8e43f0de6aa8b208c957063199164309ae1d60f0
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-11 00:06:18 -0400

    Refactored by deleting some of the overrides

--- test/game/pieces/lightgray/LightGrayPawn.scala
+++ test/game/pieces/lightgray/LightGrayPawn.scala
@@ -4,15 +4,10 @@ import game.{LightGray, Player, RelativePosition, RelativePositionRange}
 import game.pieces.Pawn
 
 abstract class LightGrayPawn extends Pawn with LightGrayPiece {
-  override val affiliation: Player = LightGray
   override val possibleMoves: Set[RelativePositionRange] =
     Set(new RelativePositionRange(new RelativePosition(0, 1),
       new RelativePosition(0, 2)))
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = false
   override val possibleCaptures: Set[RelativePositionRange] =
     Set(RelativePositionRange(new RelativePosition(-1, 1)),
       RelativePositionRange(new RelativePosition(1, 1)))
-  override val hasSpecialMoves: Boolean = true
-
 }
