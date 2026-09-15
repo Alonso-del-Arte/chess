798732aaabced9830718e8aaa9f24c382674c67d
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-14 23:29:50 -0400

    Failing canJumpOver test

--- test/game/pieces/lightgray/EmperorsPawnTest.scala
+++ test/game/pieces/lightgray/EmperorsPawnTest.scala
@@ -21,4 +21,10 @@ class EmperorsPawnTest {
     assertEquals(expected, actual)
   }
 
+  @Test def testCanJumpOver(): Unit = {
+    println("canJumpOver")
+    val msg = "Pawn shouldn't be able to jump over"
+    assert(!EmperorsPawn.canJumpOver, msg)
+  }
+
 }
