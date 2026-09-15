edd0049ca6d56f2017e1fa87f5b52607a965d1d8
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-15 00:10:15 -0400

    Removed unused import

--- test/game/pieces/lightgray/EmperorsPawnTest.scala
+++ test/game/pieces/lightgray/EmperorsPawnTest.scala
@@ -1,6 +1,6 @@
 package game.pieces.lightgray
 
-import game.{DarkGray, LightGray, RelativePosition, RelativePositionRange}
+import game.{LightGray, RelativePosition, RelativePositionRange}
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
