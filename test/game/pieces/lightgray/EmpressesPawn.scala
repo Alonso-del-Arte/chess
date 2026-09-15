e33ee3c1c50d5aeef574cf9932c48864fc67dedd
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-15 00:14:19 -0400

    Preparing to fail tests

--- test/game/pieces/lightgray/EmpressesPawn.scala
+++ test/game/pieces/lightgray/EmpressesPawn.scala
@@ -1,5 +1,21 @@
 package game.pieces.lightgray
 
+import game.{LightGray, Neutral, Player, RelativePosition,
+  RelativePositionRange}
+
 object EmpressesPawn extends LightGrayPawn with LightGrayPiece {
+  // TODO: Write a test for this
+  override val affiliation: Player = Neutral
+  // TODO: Write a test for this
+  override val possibleMoves: Set[RelativePositionRange] = Emperor.possibleMoves
+  // TODO: Write a test for this
+  override val canJumpOver: Boolean = true
+  // TODO: Write a test for this
+  override val captureSameAsMove: Boolean = true
+  // TODO: Write a test for this
+  override val possibleCaptures: Set[RelativePositionRange] =
+    Empress.possibleCaptures
+  // TODO: Write a test for this
+  override val hasSpecialMoves: Boolean = false
 
 }
