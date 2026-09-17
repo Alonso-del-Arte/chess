7ef1b167dc05c65a07b96dc8213c53b81e26d9c8
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-16 22:19:01 -0400

    Failing captureSameAsMove test

--- test/game/pieces/lightgray/HorsesPawnTest.scala
+++ test/game/pieces/lightgray/HorsesPawnTest.scala
@@ -27,4 +27,10 @@ class HorsesPawnTest {
     assert(!HorsesPawn.canJumpOver, msg)
   }
 
+  @Test def testCaptureSameAsMove(): Unit = {
+    println("captureSameAsMove")
+    val msg = "Pawn shouldn't capture same as move"
+    assert(!HorsesPawn.captureSameAsMove, msg)
+  }
+
 }
