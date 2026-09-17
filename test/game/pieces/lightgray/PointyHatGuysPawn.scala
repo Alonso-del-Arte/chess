0a6a55b074959de186dacec7530322dfe186c763
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-17 00:53:06 -0400

    Passing affiliation test

--- test/game/pieces/lightgray/PointyHatGuysPawn.scala
+++ test/game/pieces/lightgray/PointyHatGuysPawn.scala
@@ -1,10 +1,9 @@
 package game.pieces.lightgray
 
-import game.{Neutral, Player, RelativePosition, RelativePositionRange}
+import game.{LightGray, Player, RelativePosition, RelativePositionRange}
 
 object PointyHatGuysPawn extends LightGrayPawn with LightGrayPiece {
-  // TODO: Write a test for this
-  override val affiliation: Player = Neutral
+  override val affiliation: Player = LightGray
   // TODO: Write a test for this
   override val possibleMoves: Set[RelativePositionRange] = Emperor.possibleMoves
   // TODO: Write a test for this
