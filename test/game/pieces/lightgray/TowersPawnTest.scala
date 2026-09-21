e024131afca16f27168fdf3202cca2174a3700c7
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-20 23:53:33 -0400

    Failing canJumpOver test

--- test/game/pieces/lightgray/TowersPawnTest.scala
+++ test/game/pieces/lightgray/TowersPawnTest.scala
@@ -21,4 +21,10 @@ class TowersPawnTest {
     assertEquals(expected, actual)
   }
 
+  @Test def testCanJumpOver(): Unit = {
+    println("canJumpOver")
+    val msg = "Pawn shouldn't be able to jump over"
+    assert(!TowersPawn.canJumpOver, msg)
+  }
+
 }
