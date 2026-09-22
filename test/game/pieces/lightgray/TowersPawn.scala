536aa21befd9b6c1ab450946c7f0c6fa6b7f000d
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-22 01:06:01 -0400

    Refactored by deleting overrides

--- test/game/pieces/lightgray/TowersPawn.scala
+++ test/game/pieces/lightgray/TowersPawn.scala
@@ -1,17 +1,3 @@
 package game.pieces.lightgray
 
-import game.{LightGray, Player, RelativePosition, RelativePositionRange}
-
-object TowersPawn extends LightGrayPawn with LightGrayPiece {
-  override val affiliation: Player = LightGray
-  override val possibleMoves: Set[RelativePositionRange] =
-    Set(new RelativePositionRange(new RelativePosition(0, 1),
-      new RelativePosition(0, 2)))
-  override val canJumpOver: Boolean = false
-  override val captureSameAsMove: Boolean = false
-  override val possibleCaptures: Set[RelativePositionRange] =
-    Set(RelativePositionRange(new RelativePosition(-1, 1)),
-      RelativePositionRange(new RelativePosition(1, 1)))
-  override val hasSpecialMoves: Boolean = true
-
-}
+object TowersPawn extends LightGrayPawn with LightGrayPiece
