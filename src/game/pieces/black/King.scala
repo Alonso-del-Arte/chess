8e1aea2e94f9aea21ed335b5ae914511f2b48948
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-25 00:00:06 -0400

    Passing special moves test

--- src/game/pieces/black/King.scala
+++ src/game/pieces/black/King.scala
@@ -1,3 +1,9 @@
 package game.pieces.black
 
-object King extends game.pieces.King with BlackPiece
+import game.moves.{Castling, SpecialMoveSpecification}
+
+object King extends game.pieces.King with BlackPiece {
+
+  override def specialMoves: Set[SpecialMoveSpecification] = Set(Castling)
+  
+}
