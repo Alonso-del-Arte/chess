a280d3dd1f94e2f82e686b91c3551d4bc142a750
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-25 15:05:22 -0400

    validate( ) stub

--- src/game/moves/SpecialMoveSpecification.scala
+++ src/game/moves/SpecialMoveSpecification.scala
@@ -3,6 +3,17 @@ package game.moves
 import game.BoardState
 import game.pieces.Piece
 
+object SpecialMoveSpecification {
+
+  def validate(spec: SpecialMoveSpecification, boardState: BoardState,
+               piece: Piece, secondPiece: Piece): Unit = {
+    // TODO: Write tests for this. The idea here is that the trait's execute()
+    //  function will call this procedure to ensure the board state meets the
+    //  special move prerequisites and throw an exception if it doesn't
+  }
+
+}
+
 trait SpecialMoveSpecification {
 
   def meetsPrerequisites(boardState: BoardState, piece: Piece,
