339adeeede627e736d2fed89641dad9b0e177250
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-25 00:13:48 -0400

    New test class

---
+++ test/game/pieces/darkgray/EmperorTest.scala
@@ -0,0 +1,11 @@
+package game.pieces.darkgray
+
+import game.{DarkGray, ShortMoveRanges}
+import game.moves.Castling
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class EmperorTest {
+
+}
