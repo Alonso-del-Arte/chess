99e4f0236de9c0e251ef16db28144bad4919fe96
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-25 00:16:43 -0400

    Failing special moves test

--- test/game/pieces/darkgray/EmperorTest.scala
+++ test/game/pieces/darkgray/EmperorTest.scala
@@ -8,4 +8,11 @@ import org.junit.jupiter.api.Test
 
 class EmperorTest {
 
+  @Test def testSpecialMoves(): Unit = {
+    println("specialMoves")
+    val expected = Set(Castling)
+    val actual = Emperor.specialMoves
+    assertEquals(expected, actual)
+  }
+
 }
