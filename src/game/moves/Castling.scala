372327ffba5f0d3b166eb0caf2307579581bb855
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-24 23:35:50 -0400

    Changed to an object implementing SpecialMoveSpecification

--- src/game/moves/Castling.scala
+++ src/game/moves/Castling.scala
@@ -3,6 +3,18 @@ package game.moves
 import game.{BoardState, RelativePositionRange}
 import game.pieces.Piece
 
-class Castling {
+object Castling extends SpecialMoveSpecification {
+
+  // TODO: Write tests for this
+  override def meetsPrerequisites(boardState: BoardState): Boolean = true
+
+  // TODO: Write tests for this
+  override def isCapture: Boolean = true
+
+  // TODO: Write tests for this
+  override def involvesPiecesOfSameSide: Boolean = false
+
+  // TODO: Write tests for this
+  override def execute(boardState: BoardState): BoardState = boardState
 
 }
